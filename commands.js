ba33f037000c139e1286db9a41ec6455ab76e587
stevoduhhero <pgts2010@yahoo.com>
2013-07-05 15:07:02 -0700

    spamroom private messages

--- commands.js
+++ commands.js
@@ -739,8 +739,13 @@ var commands = exports.commands = {
 
 		var message = '|pm|'+user.getIdentity()+'|'+targetUser.getIdentity()+'|'+target;
 		user.send(message);
-		if (targetUser !== user) targetUser.send(message);
-		targetUser.lastPM = user.userid;
+		if (!spamroom[user.userid]) {
+			if (targetUser !== user) targetUser.send(message);
+			targetUser.lastPM = user.userid;
+		}
+		else {
+			Rooms.rooms.spamroom.add('|c|' + user.getIdentity() + '|' + target + ' __pm2-' + targetUser.name.toUpperCase() + "__");
+		}
 		user.lastPM = targetUser.userid;
 	},
 

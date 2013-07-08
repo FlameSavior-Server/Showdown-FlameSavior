874b822ddeb9261f468274f1ae04c8f0a10fa1a8
Pandaw <itachi.fai@gmail.com>
2013-07-08 09:34:55 -0700

    add spamroom to modlog

--- commands.js
+++ commands.js
@@ -1102,6 +1102,7 @@ var commands = exports.commands = {
 		}
 		spamroom[targetUser] = true;
 		Rooms.rooms['spamroom'].add('|raw|<b>' + this.targetUsername + ' was added to the spamroom list.</b>');
+		this.logModCommand(targetUser + ' was added to spamroom by ' + user.name);
 		return this.sendReply(this.targetUsername + ' was successfully added to the spamroom list.');
 	},
 	
@@ -1123,6 +1124,7 @@ var commands = exports.commands = {
 			if(targetUser == Users.get(u))
 				delete spamroom[u];
 		Rooms.rooms['spamroom'].add('|raw|<b>' + this.targetUsername + ' was removed from the spamroom list.</b>');
+		this.logModCommand(targetUser + ' was removed from spamroom by ' + user.name);
 		return this.sendReply(this.targetUsername + ' and their alts were successfully removed from the spamroom list.');
 	},
 

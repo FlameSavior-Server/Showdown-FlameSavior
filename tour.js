386fdcc77df8be72dec6afc04a308797a84293df
stevoduhhero <pgts2010@yahoo.com>
2013-07-25 15:57:01 -0700

    fix pollremind

--- tour.js
+++ tour.js
@@ -859,6 +859,7 @@ var cmds = {
 	
 	pollremind: 'pr',
 	pr: function(target, room, user) {
+		var separacion = "&nbsp;&nbsp;";
 		if (!tour[room.id].question) return this.sendReply('There is currently no poll going on.');
 		if (!this.canBroadcast()) return;
 		this.sendReply('|raw|<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font></h2><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');

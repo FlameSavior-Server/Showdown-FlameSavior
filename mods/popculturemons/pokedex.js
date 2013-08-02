10de519089b8b0ec5144fe34a06e810b6746da4f
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-08-02 14:36:51 -0700

    Create pokedex.js

---
+++ mods/popculturemons/pokedex.js
@@ -0,0 +1,6 @@
+exports.BattlePokedex = {
+megaman:{num:800,species:"Megaman",types:["Techno","Steel"],gender:"N",baseStats:{hp:95,atk:90,def:90,spa:130,spd:85,spe:110},abilities:{0:"Compoundeyes",1:"Android Heart",DW:"Download"},heightm:1.5,weightkg:82.5,color:"Blue",eggGroups:["No Eggs"],otherFormes:["megamanbomb","megamanfire"]},
+megamanbomb:{num:800,species:"Megaman-Bomb",basespecies:"Megaman",forme:"Bomb",formeletter:"B",types:["Techno","Normal"],gender:"N",baseStats:{hp:100,atk:120,def:105,spa:90,spd:100,spe:88},abilities:{0:"DynoMIGHT",1:"Android Heart",DW:"Robotic"},heightm:1.5,weightkg:82.5,color:"Purple",eggGroups:["No Eggs"]},
+megamanfire:{num:800,species:"Megaman-Fire",basespecies:"Megaman",forme:"Fire",formeletter:"F",types:["Techno","Fire"],gender:"N",baseStats:{hp:85,atk:120,def:70,spa:140,spd:70,spe:118},abilities:{0:"Flash Fire",1:"Android Heart",DW:"Exterminate"},heightm:1.5,weightkg:82.5,color:"Purple",eggGroups:["No Eggs"]},
+goku:{num:801,species:"Goku",types:["Fighting","Saiyan"],gender:"N",baseStats:{hp:73,atk:145,def:65,spa:145,spd:65,spe:135},abilities:{0:"Moxie",DW:"Iron Fist"},heightm:1.5,weightkg:82.5,color:"Purple",eggGroups:["No Eggs"]}
+};

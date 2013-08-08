9466fed437a25d7d64d2d4ed7e42600da0ae356b
Pandaw <itachi.fai@gmail.com>
2013-08-07 17:46:03 -0700

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -968,7 +968,7 @@ var commands = exports.commands = {
 	kupo: function(target, room, user){
 		if(!this.canBroadcast()|| !user.can('broadcast')) return this.sendReply('/kupo - Access Denied.');
 		if(!target) return this.sendReply('Insufficent Parameters.');
-		Rooms.rooms.lobby.add('|c|kupo|/me '+ target);
+		room.add('|c|kupo|/me '+ target);
 		this.logModCommand(user.name + ' used /kupo to say ' + target);
 	},
 	

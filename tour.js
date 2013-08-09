0df53de2b68f46240792ff87b9691909d0c4d20f
kupochu <kennybonin@gmail.com>
2013-08-09 02:58:15 -0500

    Removing a mistake on Stevo's side.

--- tour.js
+++ tour.js
@@ -383,7 +383,7 @@ var cmds = {
 
 	hotpatch: function(target, room, user) {
 		if (!target) return this.parse('/help hotpatch');
-		if (!user.can('hotpatch') && user.userid != 'slayer95' && user.userid != 'oiawesome') return false;
+		if (!user.can('hotpatch') return false;
 
 		this.logEntry(user.name + ' used /hotpatch ' + target);
 

ca672a0a669e1ac788b04832c5ac4b518951645d
Pandaw <itachi.fai@gmail.com>
2013-08-13 18:51:56 -0700

    Update command-parser.js

--- command-parser.js
+++ command-parser.js
@@ -255,6 +255,7 @@ var parse = exports.parse = function(message, room, user, connection, levelsDeep
 		delete tells[user.userid];
 	}
 
+/*
 	if (room.isGTEnforce) {
 		var strippedMessage = message.trim();
 		if (strippedMessage.slice(0,1) === '>' && strippedMessage.slice(2,3) !== '>' && strippedMessage.slice(2,3) !== '<') {
@@ -273,6 +274,7 @@ var parse = exports.parse = function(message, room, user, connection, levelsDeep
 			return false;
 		}
 	}
+*/
 
 	return message;
 };

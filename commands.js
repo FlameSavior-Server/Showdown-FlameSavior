46f7ff4a50617087adb86454d04c5460bd430633
Pandaw <itachi.fai@gmail.com>
2013-08-13 19:07:15 -0700

    Update commands.js

--- commands.js
+++ commands.js
@@ -85,7 +85,7 @@ var commands = exports.commands = {
 
 	lelon: function (target, room, user) {
 		if (!this.can('root')) return;
-		if (room.type !== 'chat' || room.auth) return this.sendReply('This command can only be used in unofficial chatrooms.');
+		if (room.type !== 'chat' || !room.auth) return this.sendReply('This command can only be used in unofficial chatrooms.');
 		if (room.isLelEnforce) return this.sendReply('The lel clause is already enforced in this room.');
 		room.isLelEnforce = true;
 		return this.sendReply('The lel clause is now being enforced in this room.');
@@ -93,7 +93,7 @@ var commands = exports.commands = {
 
 	leloff: function (target, room, user) {
 		if (!this.can('root')) return;
-		if (room.type !== 'chat' || room.auth) return this.sendReply('This command can only be used in unofficial chatrooms.');
+		if (room.type !== 'chat' || !room.auth) return this.sendReply('This command can only be used in unofficial chatrooms.');
 		if (!room.isLelEnforce) return this.sendReply('The lel clause is not being enforced in this room.');
 		room.isLelEnforce = false;
 		return this.sendReply('The lel clause is no longer being enforced in this room.');
@@ -101,7 +101,7 @@ var commands = exports.commands = {
 
 	greentexton: function (target, room, user) {
 		if (!this.can('root')) return;
-		if (room.type !== 'chat'|| room.auth) return this.sendReply('This command can only be used in chatrooms.');
+		if (room.type !== 'chat'|| !room.auth) return this.sendReply('This command can only be used in chatrooms.');
 		if (room.isGTEnforce) return this.sendReply('The greentext clause is already enforced in this room.');
 		room.isGTEnforce = true;
 		return this.sendReply('The greentext clause is now being enforced in this room.');
@@ -109,7 +109,7 @@ var commands = exports.commands = {
 
 	greentextoff: function (target, room, user) {
 		if (!this.can('root')) return;
-		if (room.type !== 'chat' || room.auth) return this.sendReply('This command can only be used in chatrooms.');
+		if (room.type !== 'chat' || !room.auth) return this.sendReply('This command can only be used in chatrooms.');
 		if (!room.isGTEnforce) return this.sendReply('The greentext clause is not being enforced in this room.');
 		room.isGTEnforce = false;
 		return this.sendReply('The greentext clause is no longer being enforced in this room.');

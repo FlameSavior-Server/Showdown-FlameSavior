5f00ebbaa5bae2216ee852b3a77472a8a4398512
小太 <nospam@xiaotai.org>
2013-08-20 22:43:07 +1000

    Remove `Data` global from tour.js

--- tour.js
+++ tour.js
@@ -405,7 +405,6 @@ var cmds = {
 			// uncache the tools.js dependency tree
 			CommandParser.uncacheTree('./tools.js');
 			// reload tools.js
-			Data = {};
 			Tools = require('./tools.js'); // note: this will lock up the server for a few seconds
 			// rebuild the formats list
 			Rooms.global.formatListText = Rooms.global.getFormatListText();

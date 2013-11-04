af6b05f4eec2202e0c733da95ca5da7976149f14
Pandaw <itachi.fai@gmail.com>
2013-11-04 14:23:22 -0800

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -962,7 +962,7 @@ var commands = exports.commands = {
 	league: function(target, room, user) {
 		if (!this.canBroadcast()) return;
 		return this.sendReplyBox('The league consists of 8 Gym Leaders, the Elite 4, and 1 Champion.<br /> ' +
-					 'Currently there the Champion position is empty.<br/>' + 
+					 'Currently, the Champion position is empty.<br/>' + 
 					 'Be the first to complete the league, and the spot is yours!<br />' +
 					 'The Battle Tower League can be found <a href="http://thebattletower.xiaotai.org/forumdisplay.php?fid=8" >here</a>.');
 	},

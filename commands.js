2815e97093bacbeb86f1b02f7a60db50c71d752c
panpawn <thebestintheworld12321@gmail.com>
2014-02-23 22:58:35 -0500

    updated /unlink

--- commands.js
+++ commands.js
@@ -2108,6 +2108,7 @@ var commands = exports.commands = {
 		if (!targetUser) {
 			return this.sendReply('User '+this.targetUsername+' not found.');
 		}
+		if targetUser.can('unlink',targetUser);
 		this.add('|unlink|' + targetUser.userid);
 		return this.privateModCommand('|html|(' + user.name + ' has made  <font color="red">' +this.targetUsername+ '</font>\'s prior links unclickable.)');
 		for (var u in targetUser.prevNames) room.add('|unlink|'+targetUser.prevNames[u]);

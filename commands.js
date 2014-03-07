dc58e0f90a4daeece279b60d754ed56a68b8dc37
jd4564 <thelittlejd09@gmail.com>
2014-03-07 02:34:35 -0400

    Improved /restart command.

--- commands.js
+++ commands.js
@@ -2654,18 +2654,22 @@ var commands = exports.commands = {
 
 	restart: function(target, room, user) {
 		if (!this.can('lockdown')) return false;
+		try {
+			var forever = require('forever'); 
+		} catch(e) {
+			return this.sendReply('/restart requires the "forever" module.');
+		}
 
 		if (!Rooms.global.lockdown) {
 			return this.sendReply('For safety reasons, /restart can only be used during lockdown.');
 		}
 
 		if (CommandParser.updateServerLock) {
-			return this.sendReply('Wait for /updateserver to finish before using /kill.');
+			return this.sendReply('Wait for /updateserver to finish before using /restart.');
 		}
 		this.logModCommand(user.name + ' used /restart');
-		var exec = require('child_process').exec;
-		exec('./restart.sh');
 		Rooms.global.send('|refresh|');
+		forever.restart('app.js');
 	},
 
 	loadbanlist: function(target, room, user, connection) {
--- restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if  pgrep node > /dev/null
-then
-	pkill node && screen -L -dmS pokemonshowdown node app.js &
-else 
-	screen -L -dmS pokemonshowdown node app.js &
-fi
-

07a72b866e3c7eeabcb8b8c1f862e1b21cab6706
panpawn <thebestintheworld12321@gmail.com>
2014-03-08 12:07:42 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -515,7 +515,7 @@ var commands = exports.commands = {
                         this.sendReplyBox('<b>Panpawn</b>:   '+admin+'  '+dev+'  '+creator+'   '+comcun+'    '+mod+'    '+vip+'');
                 }
               	if (!matched) {
-                        this.sendReply('User "'+target+'" does not have any badges.');
+                        this.sendReply('<b>'+target+'</b>: - does not have any badges.');
                 }
                
                 

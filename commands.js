4ef7e7a10c0e106e76ccc9ae7635a2e9f3ba0263
panpawn <thebestintheworld12321@gmail.com>
2014-03-10 23:42:18 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -1014,7 +1014,7 @@ var commands = exports.commands = {
 		if (!this.can('pban')) return false;
 		var target = toUserid(target);
 
-		  Users.users[target].send('|pm|~Room Request|Hello, "'+target+'".  Sorry, your recent room request has been denied.  However, you may submit another application to request a new room at any time. The reason why your room was denied was because we did\'t see a point for it on the server.  Regards, Gold Staff.'); 
+		  Users.users[target].send('|pm|~Room Request|'+target+'|Hello, "'+target+'".  Sorry, your recent room request has been denied.  However, you may submit another application to request a new room at any time. The reason why your room was denied was because we did\'t see a point for it on the server.  Regards, Gold Staff.'); 
 		
 	},
 //End new room commands

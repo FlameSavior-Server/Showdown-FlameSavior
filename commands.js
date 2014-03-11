dc504ef09b6692de2f90f834a921d079ff868851
panpawn <thebestintheworld12321@gmail.com>
2014-03-11 15:07:04 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -1015,7 +1015,12 @@ var commands = exports.commands = {
         if (target.length > 550) return this.sendReply('This new room suggestion is too long; it cannot exceed 550 characters.');
         if (!this.canTalk()) return;
         Rooms.rooms.staff.add('|html|<font size="4"><b>New Room Suggestion Submitted!</b></font><br><b>Suggested by:</b> '+user.userid+'<br><b>Suggestion</b> <i>(see /newroomquestions)</i>:<br> '+target+'');
+        
         this.sendReply('Thanks, your new room suggestion has been sent.  We\'ll review your feedback soon and get back to you. ("'+target+'")');
+	
+	for (var u in Users.users) { if (Users.users[u].isStaff) {
+		Users.users[u].send('|pm|~Staff PM|'+Users.users[u].group+Users.users[u].name+'|Attention, "'+user.userid+'" has suggested a new room.  Please meet in the staff room to talk about it.'); } 
+		}
 	},
 	roomreply: function(target, room, user) {
 		if (!target) return this.parse('/roomreply [user] - Sends a reply to [user] saying that their room was denied. ');

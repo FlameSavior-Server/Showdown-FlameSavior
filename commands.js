e3ad68f98ac1865e0ec40562c7f4204380e9a20f
panpawn <thebestintheworld12321@gmail.com>
2014-03-10 20:50:41 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -1006,7 +1006,7 @@ var commands = exports.commands = {
         if (target.indexOf('<center') > -1) return this.sendReply('HTML is not supported in this command.');
         if (target.length > 550) return this.sendReply('This new room suggestion is too long; it cannot exceed 550 characters.');
         if (!this.canTalk()) return;
-        Rooms.rooms.staff.add('|html|<font size="4"><b>New Room Suggestion</b></font><br>Suggested by: '+user.userid+'<br>Suggestion (see /newroomquestions):<br> '+target+'');
+        Rooms.rooms.staff.add('|html|<font size="4"><b>New Room Suggestion!</b></font><br>Suggested by: '+user.userid+'<br>Suggestion (see /newroomquestions):<br> '+target+'');
         this.sendReply('Thanks, your new room suggestion has been sent.  We\'ll review your feedback soon and get back to you. ("'+target+'")');
 	},
 //End new room commands

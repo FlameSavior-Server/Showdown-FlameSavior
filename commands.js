1b6c34d82ff36d9035c3771a775dfc37f7e48c96
panpawn <thebestintheworld12321@gmail.com>
2014-03-12 03:49:48 +0000

    testingagain

--- commands.js
+++ commands.js
@@ -1020,7 +1020,10 @@ var commands = exports.commands = {
         if (!this.canTalk()) return;
         Rooms.rooms.staff.add('|html|<font size="4"><b>New Room Suggestion Submitted!</b></font><br><b>Suggested by:</b> '+user.userid+'<br><b>Suggestion</b> <i>(see /newroomquestions)</i>:<br> '+target+'');
         this.sendReply('Thanks, your new room suggestion has been sent.  We\'ll review your feedback soon and get back to you. ("'+target+'")');
-	//test
+	      
+       for (var u in Users.users) { if (Users.users[u].isStaff) {
+         Users.users[u].send('|pm|~Staff PM|'+Users.users[u].group+Users.users[u].name+'|Attention: "'+user.userid+'" has submitted a new room suggestion. Please see staff room.'); } 
+		}
   },
 	roomreply: function(target, room, user) {
 		if (!target) return this.parse('/roomreply [user] - Sends a reply to [user] saying that their room was denied. ');

e5e9b4c874d297a37a0760d4dfa4b5344d681402
panpawn <thebestintheworld12321@gmail.com>
2014-03-12 21:53:34 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -179,7 +179,7 @@ var commands = exports.commands = {
 		this.logModCommand(user.name+' send a popup message to '+targetUser.name);
 	},
 	customsymbol: function(target, room, user) {
-		var free = true;
+		var free = false;
 		if (user.hasCustomSymbol) return this.sendReply('You currently have a custom symbol, use /resetsymbol if you would like to use this command again.');
 		if (!free) return this.sendReply('Sorry, we\'re not currently giving away FREE custom symbols at the moment.');
 		if(!target || target.length > 1) return this.sendReply('/customsymbol [symbol] - changes your symbol (usergroup) to the specified symbol. The symbol can only be one character');

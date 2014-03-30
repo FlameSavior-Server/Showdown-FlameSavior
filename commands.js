d3e37179f2dc521e79aa70fbf6539df158eccc19
panpawn <thebestintheworld12321@gmail.com>
2014-03-30 10:29:12 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -2136,7 +2136,7 @@ var commands = exports.commands = {
 		this.sendReplyBox('<div class="broadcast-gold" border="5"><center><img src="http://media.tumblr.com/e40eb84460e150c6b8d51b49607574f5/tumblr_inline_mjrw7vcVu51qz4rgp.gif" border="5"></center><br />' +
                                         '<b>1.</b> Welcome to Gold!<br>' +
                                         '<b>2.</b> If you should have any questions or concerns while you\'re here, than please feel free to PM a server staff member, a: Driver (%), Moderator (@), Leader (&), or, if it\'s very serious, an Administrator (~).<br>' +
-                                        '<b>3.</b> Do /events for a list of events that we do on Gold!<br>' +
+                                        '<b>3.</b> Do /events for a list of events that we do here!<br>' +
                                         '<b>4.</b> Advertising another server is an auto lock by PM or in chat.<br>' +
                                         '<b>5.</b> We hope you have fun while you\'re here! If so, then please be sure to tell your friends about us!<br>' +
                                         '<hr width="85%">' +
@@ -2260,7 +2260,7 @@ var commands = exports.commands = {
                         return connection.sendTo('lobby','|html|<div class="broadcast-gold" border="5"><center><img src="http://media.tumblr.com/e40eb84460e150c6b8d51b49607574f5/tumblr_inline_mjrw7vcVu51qz4rgp.gif" border="5"></center><br />' +
                                         '<b>1.</b> Welcome to Gold!<br>' +
                                         '<b>2.</b> If you should have any questions or concerns while you\'re here, than please feel free to PM a server staff member, a: Driver (%), Moderator (@), Leader (&), or, if it\'s very serious, an Administrator (~).<br>' +
-                                        '<b>3.</b> Do /events for a list of events that we do on Gold!<br>' +
+                                        '<b>3.</b> Do /events for a list of events that we do here!<br>' +
                                         '<b>4.</b> Advertising another server is an auto lock by PM or in chat.<br>' +
                                         '<b>5.</b> We hope you have fun while you\'re here! If so, then please be sure to tell your friends about us!<br>' +
                                         '<hr width="85%">' +

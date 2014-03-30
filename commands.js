cd89365e052db292c08a506a12bcfb416cb77f9d
panpawn <thebestintheworld12321@gmail.com>
2014-03-29 23:46:39 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -2257,7 +2257,7 @@ var commands = exports.commands = {
                                         '<b>3.</b> Do /events for a list of events that we do on Gold!<br>' +
                                         '<b>4.</b> Advertising another server is an auto lock by PM or in chat.<br>' +
                                         '<b>5.</b> We hope you have fun while you\'re here! If so, then please be sure to tell your friends about us!<br>' +
-                                        '<center><a href="http://goldserver.weebly.com/rules"><button>Rules</a></button>   |   <a href="http://goldserver.weebly.com/news"><button>News</a></button>   |   <a href="http://goldserver.weebly.com/"><button>Website</a></button></div>');
+                                        '<center><a href="http://goldserver.weebly.com/rules"><button>Rules</a></button>   |   <a href="http://goldserver.weebly.com/news"><button>News</a></button>   |   <a href="http://goldserver.weebly.com/"><button>Website</a></button>   |   <a href="http://goldserver.weebly.com/faqs"><button>FAQs</a><button></div>');
                 }
 		if (target.toLowerCase() == "staff") {
 			return connection.sendTo('staff','|html|<center><font size="7">â˜…  <img src="http://www.mydoorsign.com/img/lg/S/Staff-Room-Wall-Sign-SE-1670_bu.gif" width="200" hieght="50">  <font size="7">â˜…</center><font size="2">' +

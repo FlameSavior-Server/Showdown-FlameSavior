0de1c206a29c572124f334f1d2ebed2d05e15756
panpawn <thebestintheworld12321@gmail.com>
2014-03-29 23:02:17 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -948,6 +948,12 @@ var commands = exports.commands = {
                 '<b><font color="#4F86F7">Ace:</font></b> <font color="red">C<font color="orange">y<font color="red">n<font color="orange">d<font color="red">a<font color="orange">q<font color="red">u<font color="orange">i<font color="red">l</font><br />' +
                 '<font color="black">"Don\'t touch me when I\'m sleeping."</font></center>');
     	},
+    	spydreigon: function(target, room, user) {
+        if (!this.canBroadcast()) return;
+        this.sendReplyBox('<center><img src="http://i.imgur.com/57drGn3.jpg" width="75%"><br>' +
+                '<img src="http://fc00.deviantart.net/fs70/f/2013/102/8/3/hydreigon___draco_meteor_by_ishmam-d61irtz.png" width="75%"><br>' +
+                'You wish you were as badass as me');
+	},
     	mushy: function(target, room, user) {
         if (!this.canBroadcast()) return;
         this.sendReplyBox('<center><font size="3" color="purple">Mushy</font><br>' +

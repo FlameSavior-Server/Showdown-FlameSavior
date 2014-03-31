e0e6dfff25b08833639a81ff0323a3af605cdb76
panpawn <thebestintheworld12321@gmail.com>
2014-03-30 22:37:49 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -2263,7 +2263,7 @@ var commands = exports.commands = {
 			}
 		}
 		if (target.toLowerCase() == "lobby") {
-                        return connection.sendTo('lobby','|html|<div class="broadcast-gold" border="5"><center><img src="http://media.tumblr.com/e40eb84460e150c6b8d51b49607574f5/tumblr_inline_mjrw7vcVu51qz4rgp.gif" border="5"></center><br />' +
+                        return connection.sendTo('lobby','|html|<div class="broadcast-gold" border="5"><table border="2" cellspacing ="0" cellpadding="3"><th><center><img src="http://media.tumblr.com/e40eb84460e150c6b8d51b49607574f5/tumblr_inline_mjrw7vcVu51qz4rgp.gif" border="5"></center><br />' +
                                         '<center><b><font size="4">Welcome to Gold!</center></font><br>' +
                                         '<b>1.</b> If you should have any questions or concerns while you\'re here, than please feel free to PM a server staff member, a: Driver (%), Moderator (@), Leader (&), or, if it\'s very serious, an Administrator (~).<br>' +
                                         '<b>2.</b> Do /events for a list of events that we do here!<br>' +

b0a7c3e03d66c7e4947e2610a676671e253ee1a9
panpawn <thebestintheworld12321@gmail.com>
2014-04-01 16:11:22 -0400

    Update users.js

--- users.js
+++ users.js
@@ -1360,7 +1360,7 @@ var User = (function () {
 		}
 		
 		if (message.toLowerCase().indexOf("psim") > -1 && message.toLowerCase().indexOf("gold.psim.us") == -1 && !this.goldDev || message.toLowerCase().indexOf("play.pokemonshowdown.com/~~") > -1 && message.toLowerCase().indexOf("play.pokemonshowdown.com/~~gold") == -1 && !this.goldDev) {
-			connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising detected. Your message was not sent.</strong>');
+			connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising detected. Your message was not sent and staff have been notified.</strong>');
 			for (var u in Users.users) {
 				if (Users.users[u].group == '~' || Users.users[u].group == '&' || Users.users[u].group == '@') {
 					Users.users[u].send('|pm|~Server|'+Users.users[u].group+Users.users[u].name+'|'+connection.user.name+' triggered the advertising filter. Room: '+room.id+' Message: ' + message);

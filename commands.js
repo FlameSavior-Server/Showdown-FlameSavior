7a9d74a1243eb3eb4664c70679f73b1f56647a39
panpawn <thebestintheworld12321@gmail.com>
2014-04-01 22:17:47 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -3210,6 +3210,7 @@ var commands = exports.commands = {
 					((names.length > 1) ? 'were' : 'was') +
 					' unlocked by ' + user.name + '.');
 		return this.parse('/unspam '+target+'');
+		Rooms.rooms.room.add(''+targetUser.name+' was unlocked from talking by '+user.name+'.');
 		} else {
 			this.sendReply('User '+target+' is not locked.');
 		}

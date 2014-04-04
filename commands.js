9a53d43aaeb950dfb82e83b66539ae6c181aeee2
panpawn <thebestintheworld12321@gmail.com>
2014-04-04 16:53:40 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -12,7 +12,7 @@
  */
 var code = fs.createWriteStream('config/friendcodes.txt',{'flags':'a'});
 var studiouser = fs.createWriteStream('config/studiopermissions.txt',{'flags':'a'});
-var inShop = ['symbol', 'custom', 'animated', 'room', 'trainer', 'fix', 'declare', 'badge', 'potd', 'forcerename'];
+var inShop = ['symbol', 'custom', 'animated', 'room', 'trainer', 'fix', 'declare', 'badge', 'potd', 'forcerename', 'musicbox'];
 var closeShop = false;
 var closedShop = 0;
 var bank = exports.bank = {
@@ -790,6 +790,17 @@ var commands = exports.commands = {
 				return this.sendReply('You do not have enough bucks for this. You need ' + (price - user.money) + ' more bucks to buy ' + target + '.');
 			}
 		}
+		if (target2 === 'musicbox') {
+			price = 45;
+			if (price <= user.money) {
+				user.money = user.money - price;
+				this.sendReply('You have purchased a music box. You need to message an Admin capable of adding this (Panpawn / papew).');
+				user.canMusicBox = true;
+				this.add(user.name + ' has purchased a music box!');
+			} else {
+				return this.sendReply('You do not have enough bucks for this. You need ' + (price - user.money) + ' more bucks to buy ' + target + '.');
+			}
+		}
 		if (target2 === 'fix') {
 			price = 10;
 			if (price <= user.money) {
@@ -872,6 +883,7 @@ var commands = exports.commands = {
 			'<tr><td>Declare</td><td>You get the ability to get two declares from an Admin in lobby. This can be used for league advertisement (not server)</td><td>25</td></tr>' +
 			'<tr><td>POTD</td><td>Buys the ability to set The Pokemon of the Day!  This Pokemon will be guaranteed to show up in random battles. </td><td>45</td></tr>' +
 			'<tr><td>Forcerename</td><td>You get the ability to rename yourself to anything you want, rank symbol will remain (name should be appropriate and temporary until you logout)</td><td>10</td></tr>' +
+			'<tr><td>Musicbox</td><td>You have a command that\'s similar to a trainer card but with links to your favorite songs! (must be appropriate).</td><td>45</td></tr>' +
 			'<tr><td>Badge</td><td>You get a VIP badge and VIP status.  A VIP can change thier avatar by PM\'ing a leader at any time (they get one for FREE as well) in addition to a FREE trainer card.</td><td>200</td></tr>' +
 			'</table><br />To buy an item from the shop, use /buy [command].<br>Do /getbucks to learn more about how to obtain bucks. </center>');
 		if (closeShop) return this.sendReply('|raw|<center><h3><b>The shop is currently closed and will open shortly.</b></h3></center>');
@@ -1000,6 +1012,17 @@ var commands = exports.commands = {
 					targetUser.send(user.name + ' has given you ' + theItem + '!');
 				}
 			}
+			if (theItem === 'musicbox') {
+				if (targetUser.canMusicBox === true) {
+					return this.sendReply('This user has already bought that item from the shop... no need for another.');
+				}
+				if (targetUser.canMusicBox === false) {
+					matched = true;
+					targetUser.canMusicBox = true;
+					Rooms.rooms.lobby.add(user.name + ' has stolen a music box from the shop!');
+					targetUser.send(user.name + ' has given you ' + theItem + '!');
+				}
+			}
 			if (theItem === 'forcerename') {
 				if (targetUser.canForcerename === true) {
 					return this.sendReply('This user has already bought that item from the shop... no need for another.');
@@ -1118,6 +1141,15 @@ var commands = exports.commands = {
 			}
 			else
 				return this.sendReply('They do not have a trainer card for you to remove.');
+		}
+		else if (target === 'musicbox') {
+			if (targetUser.canMusicBox) {
+				targetUser.canMusicBox = false;
+				this.sendReply(targetUser.name + ' no longer has a music box ready to use.');
+				targetUser.send(user.name + ' has removed the music box from you.');
+			}
+			else
+				return this.sendReply('They do not have a music box for you to remove.');
 		}
 		else if (target === 'fix') {
 			if (targetUser.canFixItem) {

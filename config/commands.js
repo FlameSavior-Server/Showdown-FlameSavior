fe9334ef8210bfbe67e95cdb039aea7e26db8535
panpawn <thebestintheworld12321@gmail.com>
2014-04-07 11:24:53 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -218,6 +218,11 @@ var commands = exports.commands = {
 			if (targetUser.canForcerename) i += ' Forcerename'
 			this.sendReply('Eligible for: ' + i);
 		}
+		if (targetUser.canVIP) { 
+			var i = '';
+			if (targetUser.canVIP) i += '(VIP User)';
+			this.sendReply(i);
+		}
 		var output = 'In rooms: ';
 		var first = true;
 		for (var i in targetUser.roomCount) {

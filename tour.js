537336e23563a9129aef3eabca73a74b0783c198
panpawn <thebestintheworld12321@gmail.com>
2014-04-07 12:05:32 -0400

    Update tour.js

--- tour.js
+++ tour.js
@@ -495,7 +495,6 @@ var cmds = {
 			'7-14 Players..... 5 Gold Bucks<br>' +
 			'15 and beyond.. 6 Gold Bucks');
 		} 
-		else {
 		if (!tour.userauth(user,room)) return this.parse('/tours');
 		if (room.decision) return this.sendReply('Prof. Oak: There is a time and place for everything! You cannot do this in battle rooms.');
 		var rid = room.id;

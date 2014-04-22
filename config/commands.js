2ddf38fd7248209a64c8b295204e4c55e45ed516
panpawn <thebestintheworld12321@gmail.com>
2014-04-22 12:21:39 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -985,10 +985,10 @@ var commands = exports.commands = {
 	crowt: function(target, room, user) {
         if (!this.canBroadcast()) return;
         this.sendReplyBox('<center><img src="http://i.imgur.com/BYTR6Fj.gif  width="80" height="80">' +
-                '<img src="http://i.imgur.com/czMd1X5.gif">' +
-                '<img src="http://50.62.73.114:8000/avatars/crowt.png"><br />' +
+                '    <img src="http://i.imgur.com/czMd1X5.gif">' +
+                '    <img src="http://50.62.73.114:8000/avatars/crowt.png"><br />' +
                 '<b><font color="#4F86F7">Ace:</font></b> <font color="blue">G<font color="black">r<font color="blue">e<font color="black">n<font color="blue">i<font color="black">n<font color="blue">i<font color="black">n<font color="blue">j<font color="black">a</font><br />' +
-                '<font color="black">""It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends." - Dumbledore"</font></center>');
+                '<font color="black">"It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends." - Dumbledore"</font></center>');
 	},        
     	ransu: function(target, room, user) {
         if (!this.canBroadcast()) return;

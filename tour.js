3bb1c1e37fcc2e0fefaea828b6d8df71cb599bf4
panpawn <thebestintheworld12321@gmail.com>
2014-04-24 13:21:03 -0400

    Update tour.js

--- tour.js
+++ tour.js
@@ -1280,6 +1280,17 @@ var cmds = {
         room.addRaw('<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font size=2 color = "#939393"><small>/vote OPTION<br /><i><font size=1>Poll started by '+user.name+'</font size></i></small></font></h2><hr />' + separacion + separacion + output + '</div>');
 	},
 	
+	pr2: function(target, room, user) {
+		var separacion = "&nbsp;&nbsp;";
+		if (!tour[room.id].question) return this.sendReply('There is currently no poll going on.');
+		if (!this.canBroadcast()) return;
+
+		if (!tour[room.id].answerList[u] || tour[room.id].answerList[u].length < 1) continue;
+                output += '<button name="send" value="/vote '+tour[room.id].answerList[u]+'">'+tour[room.id].answerList[u]+'</button>&nbsp;'; 
+
+		this.sendReply('|raw|<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font></h2><hr />' + separacion + separacion + output +" &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
+	},
+	
 	pollremind: 'pr',
 	pr: function(target, room, user) {
 		var separacion = "&nbsp;&nbsp;";

58380e90cf90883b7c10ecf894b4d5b72783d984
jd4564 <thelittlejd09@gmail.com>
2014-04-24 01:31:25 -0300

    Update /data.

--- config/commands.js
+++ config/commands.js
@@ -412,7 +412,6 @@ var commands = exports.commands = {
 					data = "No Pokemon, item, move or ability named '" + target + "' was found. Showing the data of '" + newTargets[0].name + "' instead.\n";
 				}
 				data += '|c|~|/data-' + newTargets[i].searchType + ' ' + newTargets[i].name + '\n';
-				if (newTargets[i].searchType === 'pokemon') data += 'Tier: ' + template.tier + '\n';
 			}
 		} else {
 			data = "No Pokemon, item, move or ability named '" + target + "' was found. (Check your spelling?)";

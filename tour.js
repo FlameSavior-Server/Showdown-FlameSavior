709b1642eab6f60057bab2e844cad928dc867d71
panpawn <thebestintheworld12321@gmail.com>
2014-04-24 13:05:41 -0400

    Update tour.js

--- tour.js
+++ tour.js
@@ -1286,12 +1286,6 @@ var cmds = {
 		if (!tour[room.id].question) return this.sendReply('There is currently no poll going on.');
 		if (!this.canBroadcast()) return;
 		this.sendReply('|raw|<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font></h2><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
-	},
-	pr2: function(target, room, user) {
-		var separacion = "&nbsp;&nbsp;";
-		if (!tour[room.id].question) return this.sendReply('There is currently no poll going on.');
-		if (!this.canBroadcast()) return;
-		this.sendReply('|raw|<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font></h2><hr />' + separacion + separacion + " &bull; <button name="send" value="/vote">" + tour[room.id].answerList.join+'">" + tour[room.id].answerList.join(' &bull; ') + '</button></div>');
 	}
 };
 

97ffd1694de5554ee3aac5726d5d36dd1fc710c9
jd4564 <thelittlejd09@gmail.com>
2014-04-25 16:26:29 -0300

    Send a message to the user when they join a tournament.

--- tournaments/frontend.js
+++ tournaments/frontend.js
@@ -637,6 +637,7 @@ var commands = {
 			remainingTime = Math.round(seconds - 60);
 			if ((Date.now() - user[tournament.room.id].joinTime) < 60000) return this.sendReply('You have recently joined the tournamnet. To prevent joining and leaving flood, you must wait '+(remainingTime - remainingTime * 2)+' seconds before joining again.');
 			tournament.addUser(user, false, this);
+			this.sendReply('You have joined the tournament.');
 			user[tournament.room.id].joinTime = Date.now();
 		},
 		l: 'leave',

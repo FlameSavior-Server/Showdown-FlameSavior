cef4e0f7bdebbadb8144ce9ce926a7e30294fe88
panpawn <thebestintheworld12321@gmail.com>
2014-04-26 17:24:25 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1923,15 +1923,15 @@ var commands = exports.commands = {
 		var winner = '';
 		var loser= '';
 		if (player1 > player2) {
-		winner = 'The winner is <font color="green">player 1!</font>';
+		winner = 'The </b>winner</b> is <font color="green">player 1</font>!';
 		loser = 'Better luck next time, player 2!';
 		}
 		if (player1 < player2) {
-		winner = 'The winner is <font color="green">player 2!</font>';
+		winner = 'The <b>winner</b> is <font color="green">player 2</font>!';
 		loser = 'Better luck next time, player 1!';
 		}
 		if (player1 === player2) {
-		winner = 'It\'s a tie!';
+		winner = 'It\'s a <b>tie</b>!';
 		loser = 'Try again!';
 		}
 		return this.sendReplyBox('<center><font size="5">Dice Game!</font></center><br />' +

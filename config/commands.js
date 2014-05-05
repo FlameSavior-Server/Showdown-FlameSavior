6efe91685e93abb2f36a53bdb65a8ca3ecc76629
panpawn <thebestintheworld12321@gmail.com>
2014-05-05 16:45:15 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -956,6 +956,12 @@ var commands = exports.commands = {
                 '<b><font color="#4F86F7">Ace:</font></b> <font color="red">C<font color="orange">y<font color="red">n<font color="orange">d<font color="red">a<font color="orange">q<font color="red">u<font color="orange">i<font color="red">l</font><br />' +
                 '<font color="black">"Don\'t touch me when I\'m sleeping."</font></center>');
     	},
+    	comet: 'sunako',
+	cometstorm: 'sunako',
+	sunako: function(target, room, user) {
+        if (!this.canBroadcast()) return;
+        this.sendReplyBox('<center><img src="http://i.imgur.com/3uyLxHC.png"><br><font color = "0F055C"><b><i>I came from a comet.');
+	},
     	popcorn: function(target, room, user) {
         if (!this.canBroadcast()) return;
         this.sendReplyBox('<center><img src="http://www.allgeekthings.co.uk/wp-content/uploads/Popcorn.gif">');

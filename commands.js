bb01a85c328191b7e13e18dd2877910ff30afc61
jd4564 <thelittlejd09@gmail.com>
2014-05-05 23:07:41 -0300

    New poof

--- commands.js
+++ commands.js
@@ -3204,7 +3204,7 @@ var commands = exports.commands = {
 //poof functions, still not neat
 function getRandMessage(user){
 	user = escapeHTML(user.name);
-	var numMessages = 46; // numMessages will always be the highest case # + 2, as Math.floor(Math.random() * numMessages) returns a range of 0 to numMessages - 1 //increasing this will make the default appear more often
+	var numMessages = 47; // numMessages will always be the highest case # + 2, as Math.floor(Math.random() * numMessages) returns a range of 0 to numMessages - 1 //increasing this will make the default appear more often
 	var message = '~~ ';
 	switch(Math.floor(Math.random()*numMessages)){
 		case 0: message = message + user + ' knocked Cosy\'s tea out of his hand... RIP.';
@@ -3296,6 +3296,8 @@ function getRandMessage(user){
 		case 43: message = message + user + ' tried to eat Piscean!'; // bought by Piscean
 		break;
 		case 44: message = message + user + ' away!'; // bought by CrazyClown94
+		break;
+		case 45: message = message + user + ' disapparated!'; // bought by Jedi Lion Potter
 		break;
 		default: message = message + user + ' bought a poisoned Pepsi!';
 	};

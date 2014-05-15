28c23166d44459fc47b89eb5ca91a730c9a7efd8
Mike <defnottailz@gmail.com>
2014-05-14 20:54:27 -0400

    TrainerCard Update

--- config/commands.js
+++ config/commands.js
@@ -1484,9 +1484,9 @@ var commands = exports.commands = {
 	
 	lucifer: function(target, room, user) {
         if (!this.canBroadcast()) return;
-        this.sendReplyBox('<center><img src="http://24.media.tumblr.com/5be77e95c27872eb59d16b4b083bf9af/tumblr_mevo8lJhd81r5fhkdo1_500.gif" width="100">' +
-                '<img src="http://i.imgur.com/xVmosIr.png" width="320">' +
-                '<img src="http://24.media.tumblr.com/tumblr_m9bn1657Sd1qckwxco1_500.gif" width="100"><br />' +
+        this.sendReplyBox('<center><img src="http://24.media.tumblr.com/5be77e95c27872eb59d16b4b083bf9af/tumblr_mevo8lJhd81r5fhkdo1_500.gif" width="140">' +
+                '<img src="http://i.imgur.com/4pciTd7.png">' +
+                '<img src="http://24.media.tumblr.com/tumblr_m9bn1657Sd1qckwxco1_500.gif" width="140"><br />' +
                 '<b>Ace:</b> Glansey<br />' +
                 '<font color=red>I will ruin your parade with some stall action, be prepared young panda.</font></center>');
     	},

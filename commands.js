52a07d1da321d4ebdba04b298eddedc62e26604d
panpawn <thebestintheworld12321@gmail.com>
2014-05-19 17:02:09 -0400

    typo

--- commands.js
+++ commands.js
@@ -3507,7 +3507,7 @@ var commands = exports.commands = {
 		}
 		spamroom[targetUser] = true;
 		Rooms.rooms['spamroom'].add('|raw|<b>' + this.targetUsername + ' was added to the spamroom list by '+user.name+'.</b>');
-		Rooms.rooms['room'].add('|raw|<b>' + this.targetUsername + ' was added to the spamroom list by '+user.name+'.</b>)');
+		Rooms.rooms['room'].add('|raw|<b>' + this.targetUsername + ' was added to the spamroom list by '+user.name+'.</b>');
 		return this.privateModCommand('|html|(<font color="red">'+ targetUser + '</font> was added to the <button name="joinRoom" value="spamroom" target="_blank">SpamRoom</button> list by ' + user.name +'.)');
 		this.logModCommand(targetUser + ' was added to spamroom by ' + user.name);
 		return this.sendReply(this.targetUsername + ' was successfully added to the spamroom list.');

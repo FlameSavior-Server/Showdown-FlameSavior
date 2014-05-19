fc5e699a79ceea36b1062fca0f3348a2a77aa103
panpawn <thebestintheworld12321@gmail.com>
2014-05-19 16:48:14 -0400

    add in a tc

--- config/commands.js
+++ config/commands.js
@@ -963,6 +963,15 @@ var commands = exports.commands = {
                 '<b><font color="#4F86F7">Ace:</font></b> <font color="red">C<font color="orange">y<font color="red">n<font color="orange">d<font color="red">a<font color="orange">q<font color="red">u<font color="orange">i<font color="red">l</font><br />' +
                 '<font color="black">"Don\'t touch me when I\'m sleeping."</font></center>');
     	},
+    	mrbug: 'bug',
+	bug: function(target, room, user) {
+        if (!this.canBroadcast()) return;
+        this.sendReplyBox('<center><font size="5" color="darkgreen">Mr. Bug</font><br />' +
+		'<img src="http://media.pocketmonsters.net/characters/2127.png" width="50%" align="left">'+ 
+		'<img src="https://1-media-cdn.foolz.us/ffuuka/board/vp/thumb/1379/30/1379304844529s.jpg" width="50%" align="right"><br />' +
+		'"delelele woooooop"<br />' +
+		'Ace: kriketot');
+    	},
     	comet: 'sunako',
 	cometstorm: 'sunako',
 	sunako: function(target, room, user) {

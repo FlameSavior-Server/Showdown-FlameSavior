5f33a7bf0441df47de9d8b26a0fe49f1b7e147b6
jd4564 <thelittlejd09@gmail.com>
2014-05-20 17:06:49 -0300

    Add NU Monotype.

--- config/formats.js
+++ config/formats.js
@@ -1048,6 +1048,13 @@ exports.Formats = [
 		banlist: ['UU', 'BL2']
 	},
 	{
+		name: "NU Monotype",
+		section: "Monotype",
+
+		ruleset: ['RU (beta)', 'Same Type Clause'],
+		banlist: ['RU', 'BL3']
+	},
+	{
 		name: "LC Monotype",
 		section: "Monotype",
 

58784d4d4a127011f71e549111ea230a81953966
panpawn <thebestintheworld12321@gmail.com>
2014-06-09 18:46:03 -0400

    Update tour.js

--- tour.js
+++ tour.js
@@ -1282,13 +1282,6 @@ var cmds = {
         room.addRaw('<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font size=2 color = "#939393"><small>/vote OPTION<br /><i><font size=1>Poll started by '+user.name+'</font size></i></small></font></h2><hr />' + separacion + separacion + output + '</div>');
 	},
 	
-	prtest: function(target, room, user) {
-                var separacion = "&nbsp;&nbsp;";
-                if (!tour[room.id].question) return this.sendReply('There is currently no poll going on.');
-                if (!this.canBroadcast()) return;
-                var output += '<button name="send" value="/vote '+tour[room.id].answerList[u]+'">'+ tour[room.id].answerList.join(' &bull; ')+'</button>&nbsp;';
-                this.sendReply('|raw|<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font class="closebutton" size=1><small>/vote OPTION</small></font></h2><hr />' + separacion + separacion + " &bull; " + output + '</div>');
-        },
 	
 	pollremind: 'pr',
 	pr: function(target, room, user) {

a84995650e79bd42541bb6494ec20a8f6876e4d5
jd4564 <thelittlejd09@gmail.com>
2014-06-13 20:45:59 -0300

    Fix a tc

--- config/commands.js
+++ config/commands.js
@@ -1641,7 +1641,7 @@ var commands = exports.commands = {
     	this.sendReplyBox('<center><img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados-shiny/thundurus.gif">' +
     		'<img src="http://i.imgur.com/0LZxxeR.png">' +
     		'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados-shiny/thundurus-therian.gif"><br />' +
-    		'<img src="http://i.imgur.com/OdXXJCa.jpg" width="100"><br />'
+    		'<img src="http://i.imgur.com/OdXXJCa.jpg" width="100"><br />' +
     		'<b>Ace:</b> Mega Ampharos<br />' +
     		'Jack of all trades, Master at none. You\'re still predicting? I\'ve already won.</center>');
     	},

14079919a723003e3be1f94f8ff02a813faa4cfd
panpawn <thebestintheworld12321@gmail.com>
2014-06-16 20:26:28 -0400

    Update chat-plugins.js

--- chat-plugins.js
+++ chat-plugins.js
@@ -287,7 +287,9 @@ var plugins = exports.plugins = {
       				}
       				if (target === 'off' || target === 'disable' || target === 'reset') {
       					if (!this.can('declare', null, room)) return;
-      					if (!room.qotd) return this.sendReply("The Quote of the Day has already been reset.");
+      					if (!room.qotd) {
+      						return this.sendReply("The Quote of the Day has already been reset.");
+      					}
       					this.logModCommand(Tools.escapeHTML(user.name) + " has reset the Quote of the Day.");
       					room.addRaw("The Quote of the Day was reset by " + Tools.escapeHTML(user.name) + ".");
       					room.quoteOn = false;

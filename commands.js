3fe252d61975c2c4cc1d62202c99cfd93e937152
panpawn <thebestintheworld12321@gmail.com>
2014-07-03 12:25:59 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -623,7 +623,7 @@ var commands = exports.commands = {
 	givebucks: function(target, room, user) {
 		if(!user.can('pban')) return this.sendReply('You do not have enough authority to do this.');
 		if(!target) return this.parse('/help givebucks');
-		if (target.indexOf('tailz,') > -1) {
+		if (target.indexOf('tailz,'|| 'Tailz,') > -1) {
 			return this.sendReply('Wow.  Congrats, you actually have some balls, kupo.')
 		}
 		if (target.indexOf(',') != -1) {

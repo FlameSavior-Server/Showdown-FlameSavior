b8e8366fba92913fa11a2d808e91fd4bc09dd197
Mike <defnottailz@gmail.com>
2014-07-05 23:50:36 -0400

    TrainerCard Update

--- config/commands.js
+++ config/commands.js
@@ -2566,7 +2566,7 @@ var commands = exports.commands = {
 	ghast: function(target, room, user) {
 		if (!this.canBroadcast()) return;
 		this.sendReplyBox('<center><img src="http://i.imgur.com/h3GJIh4.gif">' +
-			'<img src="http://i.imgur.com/Hia14zw.png" width="370">' +
+			'<img src="http://i.imgur.com/Hia14zw.png" width="350">' +
 			'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/aegislash.gif"><br />' +
 			'<b>Ace:</b> Gengar<br />' +
 			'I am darkness itself. I am your nightmares, your shadows, & everything you fear.</center>');

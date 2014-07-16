fcfd9b5cfc4c604acc80f2eb0205f2bcad6997b0
jd4564 <thelittlejd09@gmail.com>
2014-07-15 23:16:38 -0300

    Don't mute users when adding them to shadow ban room.

--- commands.js
+++ commands.js
@@ -1301,7 +1301,7 @@ var commands = exports.commands = {
 		}
 		this.privateModCommand("(" + user.name + " has added to the shadow ban user list: " + targets.join(", ") + (reason ? " (" + reason + ")" : "") + ")");
 
-		return this.parse('/' + action + ' ' + this.targetUser.userid + ',' + reason);
+		//return this.parse('/' + action + ' ' + this.targetUser.userid + ',' + reason);
 	},
 
 	unspam: 'unshadowban',

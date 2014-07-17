558affda9d3c6c28384ec65f4a738c3dac40e60a
Mike <defnottailz@gmail.com>
2014-07-16 20:25:41 -0400

    TrainerCard Update

--- config/commands.js
+++ config/commands.js
@@ -1104,7 +1104,7 @@ var commands = exports.commands = {
 	
 	saago: function(target, room, user) {
         if (!this.canBroadcast()) return;
-        this.sendReplyBox('<center><img src="http://fc01.deviantart.net/fs26/f/2008/160/3/8/Clear_Image_of_Shaymi_Sky_Form_by_Atsui_Gure.png"><br />' +
+        this.sendReplyBox('<center><img src="http://i.imgur.com/v3wWa5Z.jpg"><br />' +
                 'Everything you\'ve ever wanted is on the other side of fear.</center>');
 	},
 	

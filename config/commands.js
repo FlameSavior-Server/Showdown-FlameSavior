93b7759f446665536a2f7fa157abeb015c9d67b3
Tailz <defnottailz@gmail.com>
2014-08-08 23:21:49 -0400

    MusicBox

--- config/commands.js
+++ config/commands.js
@@ -1554,6 +1554,17 @@ var commands = exports.commands = {
            	'<img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="left"><img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="right"><br><br>');
    	},
 
+	hopebox7: function(target, room, user) {
+       	if (!this.canBroadcast()) return;
+       	this.sendReply('|raw|<img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="left"><img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="right">' +
+           	'<center><h1><font color="tael"><u>Hope\'s Music Box</u></color></h1></center>' +
+           	'<center><b><a href="https://www.youtube.com/watch?v=-fDcvxzgCBE">Crisis City Modern</a><br />' +
+			'<center><b><a href="https://www.youtube.com/watch?v=yHm2b2jd1Ts">City Escape Classic</a><br />' +
+			'<center><b><a href="https://www.youtube.com/watch?v=9nw7usYYTaU">Rooftop Run Classic</a><br />' +
+			'<center><b><a href="https://www.youtube.com/watch?v=3OmmxC6FutA">Shadow For True Story</a><br /></center>' +
+           	'<img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="left"><img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="right"><br><br>');
+    },
+
 	highbox: function(target, room, user) {
 		if (!this.canBroadcast()) return;
 		this.sendReply('|raw|<img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="left"><img src="http://i.imgur.com/Df5hZ9S.png" width="30" height="30" align="right">' +

8a8386a5f4977e5f76db89b27f2839d1a2cc6640
panpawn <thebestintheworld12321@gmail.com>
2014-08-22 17:56:53 -0400

    add badges

--- config/commands.js
+++ config/commands.js
@@ -159,6 +159,7 @@
  *
  * @license MIT license
  */
+var badges = fs.createWriteStream('config/badges.txt',{'flags':'a'});
 var commands = exports.commands = {
 
 	ip: 'whois',
@@ -2598,7 +2599,155 @@ var commands = exports.commands = {
 		}
 		*/
 	},
-	
+	removebadge: function(target, room, user) {
+		if (!this.can('hotpatch')) return false;
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!target) return this.sendReply('/removebadge [user], [badge] - Removes a badge from a user.');
+		if (!targetUser) return this.sendReply('There is no user named '+this.targetUsername+'.');
+		var self = this;
+		var type_of_badges = ['admin','dev','vip','artist','mod','leader','champ','creator','concun','twinner','goodra','league'];
+		if (type_of_badges.indexOf(target) > -1 == false) return this.sendReply('The badge '+target+' is not a valid badge.');
+		fs.readFile('config/badges.txt','utf8', function(err, data) {
+			if (err) console.log(err);
+			var match = false;
+			var currentbadges = '';
+			var row = (''+data).split('\n');
+			var line = '';
+			for (var i = row.length; i > -1; i--) {
+				if (!row[i]) continue;
+				var split = row[i].split(':');
+				if (split[0] == targetUser.userid) {
+					match = true;
+					currentbadges = split[1];
+					line = row[i];
+				}
+			}
+			if (match == true) {
+				if (currentbadges.indexOf(target) > -1 == false) return self.sendReply(currentbadges);//'The user '+targetUser+' does not have the badge.');
+				var re = new RegExp(line, 'g');
+				currentbadges = currentbadges.replace(target,'');
+				var newdata = data.replace(re, targetUser.userid+':'+currentbadges);
+				fs.writeFile('config/badges.txt',newdata, 'utf8', function(err, data) {
+					if (err) console.log(err);
+					return self.sendReply('You have removed the badge '+target+' from the user '+targetUser+'.');
+				});
+			} else {
+				return self.sendReply('There is no match for the user '+targetUser+'.');
+			}
+		});
+	},
+	givebadge: function(target, room, user) {
+		if (!this.can('hotpatch')) return false;
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser) return this.sendReply('There is no user named '+this.targetUsername+'.');
+		if (!target) return this.sendReply('/givebadge [user], [badge] - Gives a badge to a user. Requires: &~');
+		var self = this;
+		var type_of_badges = ['admin','dev','vip','mod','artist','leader','champ','creator','comcun','twinner','league'];
+		if (type_of_badges.indexOf(target) > -1 == false) return this.sendReply('Ther is no badge named '+target+'.');
+		fs.readFile('config/badges.txt', 'utf8', function(err, data) {
+			if (err) console.log(err);
+			var currentbadges = '';
+			var line = '';
+			var row = (''+data).split('\n');
+			var match = false;
+			for (var i = row.length; i > -1; i--) {
+				if (!row[i]) continue;
+				var split = row[i].split(':');
+				if (split[0] == targetUser.userid) {
+					match = true;
+					currentbadges = split[1];
+					line = row[i];
+				}
+			}
+			if (match == true) {
+				if (badges.indexOf(target) > -1) return self.sendReply('The user '+targerUser+' already has the badge '+target+'.');
+				var re = new RegExp(line, 'g');
+				var newdata = data.replace(re, targetUser.userid+':'+currentbadges+target);
+				fs.writeFile('config/badges.txt', newdata, function(err, data) {
+					if (err) console.log(err);
+					self.sendReply('You have given the badge '+target+' to the user '+targetUser+'.');
+					targetUser.send('You have recieved the badge '+target+' from the user '+user.userid+'.');
+				});
+			} else {
+				fs.appendFile('config/badges.txt','\n'+targetUser.userid+':'+target, function(err) {
+					if (err) console.log(err);
+					self.sendReply('You have given the badge '+target+' to the user '+targetUser+'.');
+					targetUser.send('You have recieved the badge '+target+' from the user '+user.userid+'.');
+				});
+			}
+		})
+	},
+	badgelist: function(target, room, user) {
+                if (!this.canBroadcast()) return;
+                var admin = '<img src="http://www.smogon.com/media/forums/images/badges/sop.png" title="Server Administrator">';
+                var dev = '<img src="http://www.smogon.com/media/forums/images/badges/factory_foreman.png" title="Gold Developer">';
+                var creator = '<img src="http://www.smogon.com/media/forums/images/badges/dragon.png" title="Server Creator">';
+                var comcun = '<img src="http://www.smogon.com/media/forums/images/badges/cc.png" title="Community Contributor">';
+                var leader = '<img src="http://www.smogon.com/media/forums/images/badges/aop.png" title="Server Leader">';
+                var mod = '<img src="http://www.smogon.com/media/forums/images/badges/pyramid_king.png" title="Exceptional Staff Member">';
+                var league ='<img src="http://www.smogon.com/media/forums/images/badges/forumsmod.png" title="Successful League Owner">';
+                var champ ='<img src="http://www.smogon.com/media/forums/images/badges/forumadmin_alum.png" title="Goodra League Champion">';
+                var artist ='<img src="http://www.smogon.com/media/forums/images/badges/ladybug.png" title="Artist">';
+                var twinner='<img src="http://www.smogon.com/media/forums/images/badges/spl.png" title="Badge Tournament Winner">';
+                var vip ='<img src="http://www.smogon.com/media/forums/images/badges/zeph.png" title="VIP">';
+ 		return this.sendReplyBox('<b>List of Gold Badges</b>:<br>   '+admin+'    '+dev+'  '+creator+'   '+comcun+'    '+mod+'    '+leader+'    '+league+'    '+champ+'    '+artist+'    '+twinner+'    '+vip+' <br>--Hover over them to see the meaning of each.<br>--Get a badge and get a FREE custom avatar!<br>--Click <a href="http://goldserver.weebly.com/badges.html">here</a> to find out more about how to get a badge.');
+	},
+	badges: 'badge',
+    	badge: function(target, room, user) {
+            	if (!this.canBroadcast()) return;
+            	if (target == '') target = user.userid;
+            	target = this.splitTarget(target);
+            	var targetUser = this.targetUser;
+            	var matched = false; 
+            	if (!targetUser) return false;
+            	var admin = '<img src="http://www.smogon.com/media/forums/images/badges/sop.png" title="Server Administrator">';
+            	var dev = '<img src="http://www.smogon.com/media/forums/images/badges/factory_foreman.png" title="Gold Developer">';
+            	var creator = '<img src="http://www.smogon.com/media/forums/images/badges/dragon.png" title="Server Creator">';
+            	var comcun = '<img src="http://www.smogon.com/media/forums/images/badges/cc.png" title="Community Contributor">';
+            	var leader = '<img src="http://www.smogon.com/media/forums/images/badges/aop.png" title="Server Leader">';
+            	var mod = '<img src="http://www.smogon.com/media/forums/images/badges/pyramid_king.png" title="Exceptional Staff Member">';
+            	var league ='<img src="http://www.smogon.com/media/forums/images/badges/forumsmod.png" title="Successful League Owner">';
+            	var champ ='<img src="http://www.smogon.com/media/forums/images/badges/forumadmin_alum.png" title="Goodra League Champion">';
+            	var artist ='<img src="http://www.smogon.com/media/forums/images/badges/ladybug.png" title="Artist">';
+                var twinner='<img src="http://www.smogon.com/media/forums/images/badges/spl.png" title="Badge Tournament Winner">';
+                var vip ='<img src="http://www.smogon.com/media/forums/images/badges/zeph.png" title="VIP">';
+                var self = this;
+                fs.readFile('config/badges.txt', 'utf8', function(err, data) {
+                	if (err) console.log(err);
+                	var row = (''+data).split('\n');
+                	var match = false;
+                	var badges;
+                	for (var i = row.length; i > -1; i--) {
+                		if (!row[i]) continue;
+                		var split = row[i].split(':');
+                		if (split[0] == targetUser.userid) {
+                			match = true;
+                			currentbadges = split[1];
+                		}
+                	}
+                	if (match == true) {
+                		var badgelist = '';
+                		if (currentbadges.indexOf('admin') > -1) badgelist+=' '+admin;
+                		if (currentbadges.indexOf('dev') > -1) badgelist+=' '+dev;
+                		if (currentbadges.indexOf('creator') > -1) badgelist+=' '+creator;
+                		if (currentbadges.indexOf('comcun') > -1) badgelist+=' '+comcun;
+                		if (currentbadges.indexOf('leader') > -1) badgelist+=' '+leader;
+                		if (currentbadges.indexOf('mod') > -1) badgelist+=' '+mod;
+                		if (currentbadges.indexOf('league') > -1) badgelist+=' '+league;
+                		if (currentbadges.indexOf('champ') > -1) badgelist+=' '+champ;
+                		if (currentbadges.indexOf('artist') > -1) badgelist+=' '+artist;
+                		if (currentbadges.indexOf('twinner') > -1) badgelist+=' '+twinner;
+                		if (currentbadges.indexOf('vip') > -1) badgelist+=' '+vip;
+                		self.sendReplyBox(targetUser.userid+"'s badges: "+badgelist);
+                		room.update();
+                	} else {
+                		self.sendReplyBox('User '+targetUser.userid+' has no badges.');
+                		room.update();
+                	}
+                });
+        },
 	gamble: 'dicegame',
 	wager: 'dicegame',
 	rollgame: 'dicegame',
@@ -2628,16 +2777,18 @@ var commands = exports.commands = {
 		}
 		
                 var results = '';
-		var rand = Math.floor(200 * Math.random()) + 1;
-		if (rand <= 100) {
-		results = this.add('|c|'+user.name+'|I\'m gambling!');
+		var player = Math.floor(6 * Math.random()) + 1;
+		var comp = Math.floor(6 * Math.random()) + 1;
+		if (player < comp) {
+		this.sendReplyBox('
+			<b>)
 		results = this.add('|c|~GoldBucks|.custom /tb '+user.name+','+target+'');
 		}
-		if (rand >= 101) {
+		if (rand >= 1001) {
 		results = this.add('|c|'+user.name+'|I\'m gambling!');	
 		results = this.add('|c|~GoldBucks|.custom /removebucks '+user.name+','+target+'');
 		}
-		if (rand == 200) {
+		if (rand == 2000) {
                 var jackpot = (target * 20);
                 results = this.add('|c|'+user.name+'|I\'m gambling!');
 		results = this.add('|c|~GoldBucks|**'+user.name+' has hit the jackpot!  That means they have won 20 times their wager! ('+jackpot+')**'); 

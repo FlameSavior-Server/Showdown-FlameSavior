57f1a144d9d98f685048e12ace999c36bb81304a
Tailz <defnottailz@gmail.com>
2014-08-28 15:01:50 -0400

    I'm Bad

--- config/commands.js
+++ config/commands.js
@@ -2595,10 +2595,10 @@ var commands = exports.commands = {
 			'<b><font face="Comic Sans MS" color="#52CC52">Ace:</font><img src="http://blog.mobileroadie.com/wp-content/uploads/2012/04/CaseStudy_042312_MLG-450x340.jpg" width="50">' +
 			'<font face="Comic Sans MS" size="3">The Neo Void</b>' +
 			'<img src="http://i435.photobucket.com/albums/qq72/L0N6H0RN/mdewTAR.png" width="60"><br />' +
-			'<b>Quote: </b>"<b>ILL</b> fucking rek <b>U M</b>8, <b>I</b> swear on me <b>NA</b>n, ill shoo<b>T</b> u wif me qu<b>I</b>ckscope.<br />' +
-			'<img src="http://25.media.tumblr.com/tumblr_mc351hVQPQ1qf021po1_500.gif" width="35">' +
+			'<b>Quote: </b><b>ILL</b> fucking rek <b>U M</b>8, <b>I</b> swear on me <b>NA</b>n, ill shoo<b>T</b> u wif me qu<b>I</b>ckscope.<br />' +
+			'<img src="http://25.media.tumblr.com/tumblr_mc351hVQPQ1qf021po1_500.gif" width="30">' +
 			'<button name="send" value="/transferbucks The Neo Void, 1">DONATE 2 ME OR IL REPOT U M8. IF U DON HAV MUNNY, GIT GUD SCRUB</button>' +
-			'<img src="http://25.media.tumblr.com/tumblr_mc351hVQPQ1qf021po1_500.gif" width="35"></center>'
+			'<img src="http://25.media.tumblr.com/tumblr_mc351hVQPQ1qf021po1_500.gif" width="30"></center>'
 		);
 	},
 

48d17345df47c510d621d2896fd5571d26a0727e
chalenged <chalenged@users.noreply.github.com>
2014-09-15 16:11:13 -0700

    Add target for hex
    allows /hex to get the hex for specified names i.e. /hex sciz

--- commands.js
+++ commands.js
@@ -2762,12 +2762,12 @@ var commands = exports.commands = {
 		if (!this.canBroadcast()) return;
         	if (!this.canTalk()) return;
 		if (target) {
-			if (toId(target) === 'panpawn') {
-				return this.sendReplyBox('<b><font color="#DA9D01">'+target+'</font></b>.  The hexcode for '+target+'\'s name color is: #DA9D01".');
-				return;
-			}
-			return this.sendReplyBox('<b><font color="'+hashColor(''+toId(target)+'')+'">'+target+'</font></b>.  The hexcode for '+target+'\'s name color is: '+hashColor(''+toId(target)+'')+'.');
+		if (toId(target) === 'panpawn') {
+			return this.sendReplyBox('<b><font color="#DA9D01">'+Tools.escapeHTML(target)+'</font></b>.  The hexcode for '+Tools.escapeHTML(target)+'\'s name color is: #DA9D01.');
 			return;
+		}
+		return this.sendReplyBox('<b><font color="'+hashColor(''+toId(target)+'')+'">'+Tools.escapeHTML(target)+'</font></b>.  The hexcode for '+Tools.escapeHTML(target)+'\'s name color is: '+hashColor(''+toId(target)+'')+'.');
+		return;
 		} 
 		if (user.userid === 'panpawn') {
 			return this.sendReplyBox('Hello, <b><font color="#DA9D01">'+user.name+'</b></font>.  Your hexcode for your name color is: #DA9D01.');

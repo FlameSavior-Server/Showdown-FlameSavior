d2838358e1c81e75a17f1e1f55231109a05fc856
panpawn <thebestintheworld12321@gmail.com>
2014-09-21 19:45:51 -0400

    whoops

--- commands.js
+++ commands.js
@@ -537,7 +537,7 @@ var commands = exports.commands = {
   		if(!target || target.length > 1) return this.sendReply('/customsymbol [symbol] - changes your symbol (usergroup) to the specified symbol. The symbol can only be one character');
   		
   		var a = target;
-		var banned Symbols = ['+','$','%','‽','!','★','@','&','~','#','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','卐','|'];  		if (a.indexOf(bannedSymbols)) return this.sendReply('Sorry, but you cannot change your symbol to this for safety/stability reasons.');
+		var bannedSymbols = ['+','$','%','‽','!','★','@','&','~','#','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','卐','|'];  		if (a.indexOf(bannedSymbols)) return this.sendReply('Sorry, but you cannot change your symbol to this for safety/stability reasons.');
   		if (a.indexOf(bannedSymbols)) return this.sendReply('Sorry, but you cannot change your symbol to this for safety/stability reasons.');
   		
   		user.getIdentity = function(){

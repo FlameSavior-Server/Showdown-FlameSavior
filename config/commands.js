a1cc5979d5e922125e76e05ef6c8ec91a60af6cd
jd4564 <thelittlejd09@gmail.com>
2014-09-22 20:24:58 -0300

    Remove leaders from /groups

--- config/commands.js
+++ config/commands.js
@@ -971,7 +971,7 @@ var commands = exports.commands = {
 			"+ <b>Voice</b> - They can use ! commands like !groups, and talk during moderated chat<br />" +
 			"% <b>Driver</b> - The above, and they can mute. Global % can also lock users and check for alts<br />" +
 			"@ <b>Moderator</b> - The above, and they can ban users<br />" +
-			"&amp; <b>Leader</b> - They can do almost anything, such as create rooms and manage bucks<br />" +
+			//"&amp; <b>Leader</b> - They can do almost anything, such as create rooms and manage bucks<br />" +
 			"# <b>Room Owner</b> - They are leaders of the room and can almost totally control it<br />" +
 			"~ <b>Administrator</b> - They can do anything, like change what this message says"
 		);

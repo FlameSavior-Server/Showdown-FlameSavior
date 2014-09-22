e3a9fda2eda009e093ca4ed73795df696b1356fd
panpawn <thebestintheworld12321@gmail.com>
2014-09-21 20:02:21 -0400

    jajaj

--- config/commands.js
+++ config/commands.js
@@ -3325,7 +3325,7 @@ var commands = exports.commands = {
 
 						reloadCustomAvatars();
 						this.sendReply(userid + "'s custom avatar has been set.");
-						Users.messageSeniorStaff(parts[1]+' has received a custom avatar from '+user.name);
+						Rooms.rooms.staff.add(parts[1]+' has received a custom avatar from '+user.name);
 					}).bind(this));
 					break;
 				case 'rem':

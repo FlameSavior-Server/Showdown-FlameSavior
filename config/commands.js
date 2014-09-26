523d3f408dde9866f5808529616984314b8f4ee7
Tailz <defnottailz@gmail.com>
2014-09-26 15:39:06 -0400

    Fix a Tc

--- config/commands.js
+++ config/commands.js
@@ -1228,9 +1228,10 @@ var commands = exports.commands = {
 
 	tesarand: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('<b><center><font color="purple"><font size="6">Tesarand</b></font>' +
-			'<img src="http://www.bonappetit.com/wp-content/uploads/2013/03/james-deen-646.jpg" height="200" width="300">' +
-			'<b>Ace: Dragonite<br>"It tastes like glitter.... Rad.”</b><br></center>');
+		this.sendReplyBox('<center><b><font size="6" color="purple">Tesarand</b></font><br />' +
+			'<img src="http://www.bonappetit.com/wp-content/uploads/2013/03/james-deen-646.jpg" height="200" width="300"><br />' +
+			'<b>Ace: Dragonite<br />' +
+			'"It tastes like glitter.... Rad.”</b><br></center>');
 	},
 	
 	shrew: function(target, room, user) {

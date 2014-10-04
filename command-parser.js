20ef943f55d8628b343fe2fff3c6b7b66b79d4fd
SilverTactic <brahadeeshk@gmail.com>
2014-10-04 20:20:11 +0530

    Update command-parser.js

--- command-parser.js
+++ command-parser.js
@@ -421,32 +421,6 @@ var parse = exports.parse = function(message, room, user, connection, levelsDeep
                     return target1;
                 }
             },
-
-            getavy: function(name) {
-                if (Users.get(name)) {
-                    if (Users.get(name).avatar.length > 3) {
-                        return '<img src = "//thearchonleague.no-ip.org:8000/avatars/' + Users.get(name).avatar + '">'
-                    } else {
-                        return '<img src = "//play.pokemonshowdown.com/sprites/trainers/' + Users.get(name).avatar + '.png">'
-                    }
-                } else {
-                    return '';
-                }
-            },
-
-            cardPrint: function(name, target, target1) {
-                var br = '<br/>';
-                if (target.lastIndexOf('<br/>') === target.length - 5) br = ''
-                if (!Users.get(name)) {
-                    return this.sendReplyBox(target + br + '<b>Last seen:</b> ' + this.lastSeen(name) + ' ago');
-                } else {
-                    if (!Users.get(name).connected) {
-                        return this.sendReplyBox(target + br + '<b>Last seen:</b> ' + this.lastSeen(name) + ' ago');
-                    } else {
-                        return this.sendReplyBox(target + target1);
-                    }
-                }
-            },
             
             getLastIdOf: function(user) {
                 if (typeof user === 'string') user = Users.get(user);

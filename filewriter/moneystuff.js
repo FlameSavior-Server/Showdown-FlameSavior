dc70c565d488b130eee8505c3b02e27d7081ac1a
SilverTactic <brahadeeshk@gmail.com>
2014-10-04 11:56:27 +0530

    Update moneystuff.js

--- filewriter/moneystuff.js
+++ filewriter/moneystuff.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var moneyStuff = exports.moneyStuff = {
 
     checkAmt: function(name, target) {
-        var userdata = fs.readFileSync('config/userdata.json');
+        var userdata = fs.readFileSync('infofiles/userdata.json');
         var amt = JSON.parse(userdata);
         if (!amt[toId(name)]) return 0;
         if (amt[toId(name)][target]) {
@@ -13,29 +13,29 @@ var moneyStuff = exports.moneyStuff = {
     },
 
     giveAmt: function(name, target, target1) {
-        var userdata = fs.readFileSync('config/userdata.json');
+        var userdata = fs.readFileSync('infofiles/userdata.json');
         var amt = JSON.parse(userdata);
         if (!amt[toId(name)]) amt[toId(name)] = {};
         if (!amt[toId(name)][target]) {
             amt[toId(name)][target] = target1;
             var finished = JSON.stringify(amt, null, 1);
-            fs.writeFile('config/userdata.json', finished);
+            fs.writeFile('infofiles/userdata.json', finished);
             return;
         }
         amt[toId(name)][target] += parseInt(target1);
         var finished = JSON.stringify(amt, null, 1);
-        fs.writeFile('config/userdata.json', finished);
+        fs.writeFile('infofiles/userdata.json', finished);
         return;
     },
 
     removeAmt: function(name, target, target1) {
-        var userdata = fs.readFileSync('config/userdata.json');
+        var userdata = fs.readFileSync('infofiles/userdata.json');
         var amt = JSON.parse(userdata);
         if (!amt[toId(name)]) amt[toId(name)] = {};
         if (!amt[toId(name)][target]) amt[toId(name)][target] = 0;
         amt[toId(name)][target] -= parseInt(target1);
         var finished = JSON.stringify(amt, null, 1);
-        fs.writeFile('config/userdata.json', finished);
+        fs.writeFile('infofiles/userdata.json', finished);
         return;
     }
 };

ea3e8133019d8c49403eed8c569ea0b2246d19bd
SilverTactic <brahadeeshk@gmail.com>
2014-10-06 17:10:14 +0530

    Update moneystuff.js

--- filewriter/moneystuff.js
+++ filewriter/moneystuff.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var moneyStuff = exports.moneyStuff = {
 
     checkAmt: function(name, target) {
-        var userdata = fs.readFileSync('infofiles/userdata.json');
+        var userdata = fs.readFileSync('config/userdata.json');
         var amt = JSON.parse(userdata);
         if (!amt[toId(name)]) return 0;
         if (amt[toId(name)][target]) {
@@ -13,40 +13,41 @@ var moneyStuff = exports.moneyStuff = {
     },
 
     giveAmt: function(name, target, target1) {
-        var userdata = fs.readFileSync('infofiles/userdata.json');
+        var userdata = fs.readFileSync('config/userdata.json');
         var amt = JSON.parse(userdata);
         if (!amt[toId(name)]) amt[toId(name)] = {};
         if (!amt[toId(name)][target]) {
-            amt[toId(name)][target] = target1;
+            parseInt(amt[toId(name)][target]) = target1;
             var finished = JSON.stringify(amt, null, 1);
-            fs.writeFile('infofiles/userdata.json', finished);
+            fs.writeFile('config/userdata.json', finished);
             return;
         }
         amt[toId(name)][target] += parseInt(target1);
         var finished = JSON.stringify(amt, null, 1);
-        fs.writeFile('infofiles/userdata.json', finished);
+        fs.writeFile('config/userdata.json', finished);
         return;
     },
 
     removeAmt: function(name, target, target1) {
-        var userdata = fs.readFileSync('infofiles/userdata.json');
+        var userdata = fs.readFileSync('config/userdata.json');
         var amt = JSON.parse(userdata);
         if (!amt[toId(name)]) amt[toId(name)] = {};
         if (!amt[toId(name)][target]) amt[toId(name)][target] = 0;
-        amt[toId(name)][target] -= parseInt(target1);
+        parseInt(amt[toId(name)][target]) -= parseInt(target1);
         var finished = JSON.stringify(amt, null, 1);
-        fs.writeFile('infofiles/userdata.json', finished);
+        fs.writeFile('config/userdata.json', finished);
+        return;
+    },
+
+    transferAmt: function(user1, user2, type, amount) {
+        var userdata = fs.readFileSync('config/userdata.json');
+        var amt = JSON.parse(userdata);
+        if (!amt[toId(user2)]) amt[toId(user2)] = {};
+        if (!amt[toId(user2)][type]) amt[toId(user2)] = 0;
+        parseInt(amt[toId(user1)][type]) -= parseInt(amount);
+        parseInt(amt[toId(user2)][type]) += parseInt(amount);
+        var finished = JSON.stringify(amt, null, 1);
+        fs.writeFile('config/userdata.json', finished);
         return;
     }
-     transferAmt: function(user1, user2, type, amount) {
-	    var userdata = fs.readFileSync('infofiles/userdata.json');
-        var amt = JSON.parse(userdata);
-		if (!amt[toId(user2)]) amt[toId(user2)] = {};
-		if (!amt[toId(user2)][type]) amt[toId(user2)] = 0; 
-		amt[toId(user1)][type] -= parseInt(amount);
-		amt[toId(user2)][type] += parseInt(amount);
-		var finished = JSON.stringify(amt, null, 1);
-	    fs.writeFile('infofiles/userdata.json', finished);
-		return;
-	}
 };

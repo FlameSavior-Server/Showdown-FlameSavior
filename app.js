78ab4712523f2dfc7128353b5570c2b373203d0f
panpawn <thebestintheworld12321@gmail.com>
2014-10-11 17:30:37 -0400

    Re Implement emotes

--- app.js
+++ app.js
@@ -451,4 +451,4 @@ global.hangman = require('./hangman.js').hangman();
 try {
 	global.economy = require('./economy.js');
 } catch (e) {}
-//global.Core = require('./core.js').core;
+global.Core = require('./core.js').core;

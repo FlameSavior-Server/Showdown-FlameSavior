4ac402cd0631efefce3a587d17265a9231c3b918
SilverTactic <brahadeeshk@gmail.com>
2014-10-16 21:43:42 +0530

    Update trainercards.js

--- funstuff/trainercards.js
+++ funstuff/trainercards.js
@@ -1,4 +1,4 @@
-exports.commands {
+var trainercards = exports.trainercards = {
     //example trainer card. You don't need to copy the same thing, this trainer card is really plain and simple.
     siiilver: 'silver',
     silver: function (target, user, room) {

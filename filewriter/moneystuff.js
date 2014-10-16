901e34d53fcb3bb8ef4bd580046d5b5119e0d408
SilverTactic <brahadeeshk@gmail.com>
2014-10-16 16:15:25 +0530

    Changed file directories.

--- filewriter/moneystuff.js
+++ filewriter/moneystuff.js
@@ -15,33 +15,23 @@ var moneyStuff = exports.moneyStuff = {
     giveAmt: function(name, target, target1) {
         if (!amt[toId(name)]) amt[toId(name)] = {};
         if (!amt[toId(name)][target]) {
-            amt[toId(name)][target] = parseInt(target1);
+            amt[toId(name)][target] = parseInt(amt[toId(name)][target]) + parseInt(target1);
             var finished = JSON.stringify(amt, null, 1);
-            fs.writeFile('config/userdata.json', finished);
+            fs.writeFile('infofiles/userdata.json', finished);
             return;
         }
         amt[toId(name)][target] += parseInt(target1);
         var finished = JSON.stringify(amt, null, 1);
-        fs.writeFile('config/userdata.json', finished);
+        fs.writeFile('infofiles/userdata.json', finished);
         return;
     },
 
     removeAmt: function(name, target, target1) {
         if (!amt[toId(name)]) amt[toId(name)] = {};
         if (!amt[toId(name)][target]) amt[toId(name)][target] = 0;
-        amt[toId(name)][target] -= parseInt(target1);
+        amt[toId(name)][target] = parseInt(amt[toId(name)][target]) - parseInt(target1);
         var finished = JSON.stringify(amt, null, 1);
-        fs.writeFile('config/userdata.json', finished);
-        return;
-    },
-
-    transferAmt: function(user1, user2, type, amount) {
-        if (!amt[toId(user2)]) amt[toId(user2)] = {};
-        if (!amt[toId(user2)][type]) amt[toId(user2)] = 0;
-        amt[toId(user1)][type] -= parseInt(amount);
-        amt[toId(user2)][type] += parseInt(amount);
-        var finished = JSON.stringify(amt, null, 1);
-        fs.writeFile('config/userdata.json', finished);
+        fs.writeFile('infofiles/userdata.json', finished);
         return;
     }
 };

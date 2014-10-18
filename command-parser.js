edaade32a40f87b098b2e0aaafb9011ab0aff2d2
chalenged <chalenged@users.noreply.github.com>
2014-10-17 21:18:59 -0700

    tells

--- command-parser.js
+++ command-parser.js
@@ -379,6 +379,17 @@ var parse = exports.parse = function (message, room, user, connection, levelsDee
 		return parse(message, room, user, connection, levelsDeep + 1);
 	}
 	
+	if (user.authenticated && global.tells) {
+		var alts = user.getAlts();
+		alts.push(user.name);
+		alts.map(toId).forEach(function (user) {
+			if (tells[user]) {
+				tells[user].forEach(connection.sendTo.bind(connection, room));
+				delete tells[user];
+			}
+		});
+	}
+	
 	if (!Core.processChatData(user, room, connection, message)) return false;
 	
 	return message;

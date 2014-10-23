7b4ec1d8cb4ecd9694621889cd77c9e041cc13c8
SilverTactic <brahadeeshk@gmail.com>
2014-10-23 10:10:12 +0530

    Update profile.js

--- filewriter/profile.js
+++ filewriter/profile.js
@@ -1,38 +1,23 @@
 var fs = require('fs');
-var userdetails = fs.readFileSync('infofiles/userdetails.json');
-var a = JSON.parse(userdetails);
+var details = JSON.parse(fs.readFileSync('infofiles/userdetails.json'));
 var profile = exports.profile = {
 
     checkDetails: function(name, target) {
-        if (!a[toId(name)]) {
-            if (toId(target) == 'gender') {
-                return "Hidden";
-            } else if (toId(target) == 'fc') {
-                return "Unregistered";
-            } else if (toId(target) == 'quote') {
-                return "Get ready!";
-            } else {
-                return "Unknown";
-            }
-        } else if (!a[toId(name)][target]) {
-            if (toId(target) == 'gender') {
-                return "Hidden";
-            } else if (toId(target) == 'fc') {
-                return "Unregistered";
-            } else if (toId(target) == 'quote') {
-                return "Get ready!";
-            } else {
-                return "Unknown";
-            }
+        if (!details[toId(name)]) {
+            if (toId(target) == 'gender') return "Hidden";
+            return "Unknown";
+        } else if (!details[toId(name)][target]) {
+            if (toId(target) == 'gender') return "Hidden";
+            return "Unknown";
         } else {
-            return a[toId(name)][target];
+            return details[toId(name)][target];
         }
     },
 
     set: function(name, target, target1) {
-        if (!a[toId(name)]) a[toId(name)] = {};
-        a[toId(name)][target] = target1;
-        var set = JSON.stringify(a, null, 1);
+        if (!details[toId(name)]) details[toId(name)] = {};
+        details[toId(name)][target] = target1;
+        var set = JSON.stringify(details, null, 1);
         fs.writeFile('infofiles/userdetails.json', set);
         return;
     }

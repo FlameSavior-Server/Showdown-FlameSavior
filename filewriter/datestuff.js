58fbe69855c1f189ba8441a23b7ee5da5c7deaa1
SilverTactic <brahadeeshk@gmail.com>
2014-10-23 17:09:19 +0530

    Update datestuff.js

--- filewriter/datestuff.js
+++ filewriter/datestuff.js
@@ -28,6 +28,13 @@ var datestuff = exports.datestuff = {
     },
     
     lastSeen: function(target) {
+                var plural = function(target, target1) {
+                    if (Math.floor(target) !== 1) {
+                        return target1 = target1 + "s";
+                    } else {
+                        return target1;
+                    }
+                }
                 var lastseen = getdate(toId(target));
                 if (lastseen == 'never') return 'never';
                 var part1 = Math.floor(lastseen / 1000);
@@ -35,38 +42,38 @@ var datestuff = exports.datestuff = {
                 if (part1 >= 60) {
                     var part2 = Math.floor(part1 / 60);
                     if (part1 % 60 === 0) {
-                        var time1 = part2 + ' ' + this.plural(part2, 'minute');
+                        var time1 = part2 + ' ' + plural(part2, 'minute');
                     } else {
-                        var time1 = part2 + ' ' + this.plural(part2, 'minute') + ', ' + (part1 % 60) + ' ' + this.plural(part1 % 60, 'second');
+                        var time1 = part2 + ' ' + plural(part2, 'minute') + ', ' + (part1 % 60) + ' ' + plural(part1 % 60, 'second');
                     }
                     if (part2 >= 60) {
                         var part3 = Math.floor(part2 / 60);
                         if (part2 % 60 !== 0) {
-                            var time1 = part3 + ' ' + this.plural(part3, 'hour') + ', ' + (part2 % 60) + ' ' + this.plural(part2 % 60, 'minute');
+                            var time1 = part3 + ' ' + plural(part3, 'hour') + ', ' + (part2 % 60) + ' ' + plural(part2 % 60, 'minute');
                         } else {
-                            var time1 = part3 + ' ' + this.plural(part3, 'hour');
+                            var time1 = part3 + ' ' + plural(part3, 'hour');
                         }
                         if (part3 >= 24) {
                             var part4 = Math.floor(part3 / 24);
                             if (part3 % 24 !== 0 && (part2 % 60) !== 0) {
-                                var time1 = part4 + ' ' + this.plural(part4, 'day') + ', ' + (part3 % 24) + ' ' + this.plural(part3 % 24, 'hour') + ', ' + (part2 % 60) + ' ' + this.plural(part2 % 60, 'minute');
+                                var time1 = part4 + ' ' + plural(part4, 'day') + ', ' + (part3 % 24) + ' ' + plural(part3 % 24, 'hour') + ', ' + (part2 % 60) + ' ' + plural(part2 % 60, 'minute');
                             } else if (part3 % 24 !== 0 && (part2 % 60) === 0) {
-                                var time1 = part4 + ' ' + this.plural(part4, 'day') + ', ' + (part3 % 24) + ' ' + this.plural(part3 % 24, 'hour');
+                                var time1 = part4 + ' ' + plural(part4, 'day') + ', ' + (part3 % 24) + ' ' + plural(part3 % 24, 'hour');
                             } else if (part3 % 24 === 0 && (part2 % 60) !== 0) {
-                                var time1 = part4 + ' ' + this.plural(part4, 'day') + ', ' + (part2 % 60) + ' ' + this.plural(part2 % 60, 'minute');
+                                var time1 = part4 + ' ' + plural(part4, 'day') + ', ' + (part2 % 60) + ' ' + plural(part2 % 60, 'minute');
                             } else {
-                                var time1 = part4 + ' ' + this.plural(part4, 'day');
+                                var time1 = part4 + ' ' + plural(part4, 'day');
                             }
                             if (part4 >= 7) {
                                 var part5 = Math.floor(part4 / 7);
                                 if (part3 % 7 !== 0 && part3 % 24 !== 0) {
-                                    var time1 = part5 + ' ' + this.plural(part5, 'week') + ', ' + (part4 % 7) + ' ' + this.plural(part4 % 7, 'day') + ', ' + (part3 % 24) + ' ' + this.plural(part3 % 24, 'hour');
+                                    var time1 = part5 + ' ' + plural(part5, 'week') + ', ' + (part4 % 7) + ' ' + plural(part4 % 7, 'day') + ', ' + (part3 % 24) + ' ' + plural(part3 % 24, 'hour');
                                 } else if (part3 % 7 !== 0 && part3 % 24 === 0) {
-                                    var time1 = part5 + ' ' + this.plural(part5, 'week') + ', ' + (part4 % 7) + ' ' + this.plural(part4 % 7, 'day');
+                                    var time1 = part5 + ' ' + plural(part5, 'week') + ', ' + (part4 % 7) + ' ' + plural(part4 % 7, 'day');
                                 } else if (part3 % 7 === 0 && part3 % 24 !== 0) {
-                                    var time1 = part5 + ' ' + this.plural(part5, 'week') + ', ' + (part4 % 7) + ' ' + this.plural(part3 % 24, 'hour');
+                                    var time1 = part5 + ' ' + plural(part5, 'week') + ', ' + (part4 % 7) + ' ' + plural(part3 % 24, 'hour');
                                 } else {
-                                    var time1 = part5 + ' ' + this.plural(part5, 'week');
+                                    var time1 = part5 + ' ' + plural(part5, 'week');
                                 }
                             }
                         }

dfbd1d2ffae249a4321d2cdd8682124b1286ad34
Tailz <defnottailz@gmail.com>
2014-11-05 01:09:55 -0500

    k im bad

--- chat-plugins/trainer-cards.js
+++ chat-plugins/trainer-cards.js
@@ -8,7 +8,7 @@ exports.commands = {
 		if (room.id !== 'lobby') if (!this.canBroadcast()) return;
 		this.sendReplyBox('<img src="http://cdn.pokestache.com/2014/3/21/bc0783231f05b032187d48a4c5c73683.gif" width="160"><br />' +
 			'<center><img src="http://cdn.pokestache.com/2014/2/20/8a9e27ad681e2e9807390122e1ed9412.gif" width="130">' +
-			'<img src="http://i.imgur.com/oKOT6FZ.gif">' +
+			'<img src="http://i.imgur.com/oKOT6FZ.gif" width="280">' +
 			'<img src="http://cdn.pokestache.com/2013/12/30/9796dab47a2ff39de7abd6a93d4eb81d.gif" width="150"><br />' +
 			'<b>Ace:</b> Beedrill<br />' +
 			'"The one who brags about winning always loses"</center>'

db318698d7eb3cf7d7406abe3e8bd8b568104bbd
Blizzard <bronyanthony@gmail.com>
2014-11-07 19:52:47 -0500

    Make sure 'cloud-env' won't crash server.

--- app.js
+++ app.js
@@ -124,11 +124,11 @@ if (Config.watchconfig) {
 // Autoconfigure the app when running in cloud hosting environments:
 try {
 	var cloudenv = require('cloud-env');
+	Config.bindaddress = cloudenv.get('IP', Config.bindaddress || '');
+	Config.port = cloudenv.get('PORT', Config.port);
 } catch (e) {
+	console.log(e);
 }
-
-Config.bindaddress = cloudenv.get('IP', Config.bindaddress || '');
-Config.port = cloudenv.get('PORT', Config.port);
 
 if (process.argv[2] && parseInt(process.argv[2])) {
 	Config.port = parseInt(process.argv[2]);

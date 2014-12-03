5000391f86d21f22f37c0f13831a9c170972ccfb
Gold-Solox <amethyst.skymin@gmail.com>
2014-12-02 19:05:02 -0500

    Adding uwotm8
    I'm probably going to stick with emotes for now.

--- core.js
+++ core.js
@@ -74,6 +74,7 @@ emoticons: {
         'SaniC': 'http://i.picresize.com/images/2014/11/27/j0fmu.png',
         'fliP': 'http://i.imgur.com/IRqXlrr.png',
         'jLp': 'http://www.rockybytes.com/i/4933/happy-potter-and-the-goblet-of-fire.jpg',
+        'uwotm8': 'http://i.picresize.com/images/2014/12/01/bDLR1.png',
         //End custom ones
         
         'crtNova': 'http://static-cdn.jtvnw.net/jtv_user_pictures/emoticon-3227-src-77d12eca2603dde0-28x28.png',

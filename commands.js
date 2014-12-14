afe0a88ad8743ded8b40ba4a9e26259d2a7e7a96
panpawn <thebestintheworld12321@gmail.com>
2014-12-13 19:09:56 -0500

    new plug partners :)

--- commands.js
+++ commands.js
@@ -1274,7 +1274,7 @@ var commands = exports.commands = {
 	radio: 'plug',
 	plug: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('Gold\'s Plug.dj can be found <a href="http://plug.dj/gold-server/">here</a>.');
+		this.sendReplyBox('Gold\'s Plug.dj can be found <a href="https://plug.dj/nightcore-331">here</a>.');
 	},
 
 	ps: function(target, room, user) {

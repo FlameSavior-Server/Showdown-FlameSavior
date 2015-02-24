a9b002780cf0c1643f4f751f1e372c248029b9e5
VBlizzard <furryblizzard@gmail.com>
2015-02-23 20:28:12 -0500

    Update /declareaotd.

--- chat-plugins/aotd.js
+++ chat-plugins/aotd.js
@@ -2,6 +2,7 @@ exports.commands = {
   declareaotd: function(target, room, user) {
     if (room.id != 'lobby') return this.sendReply("The command must be used in Lobby.");
     if (!this.canBroadcast()) return false;
+    if (!this.canTalk()) return false;
     this.add('|raw|<div class="broadcast-blue"><b>AOTD has begun in GoldenrodRadioTower!! ' +
              'Join now to nominate your favorite artist for AOTD to be featured on the ' +
              'official page next to your name for a chance to win the monthly prize at the end of the month!!</b></div>'

73d57c67d9bd7dc8d9dd2ded40e060cb5d47462c
jd4564 <thelittlejd09@gmail.com>
2015-04-09 06:00:18 -0300

    Update /u and /define

--- components.js
+++ components.js
@@ -16,6 +16,8 @@ var fs = require("fs"),
     path = require("path"),
     http = require("http");
 
+var request = require('request');
+
 var bubbleLetterMap = new Map([
 	['a', '\u24D0'], ['b', '\u24D1'], ['c', '\u24D2'], ['d', '\u24D3'], ['e', '\u24D4'], ['f', '\u24D5'], ['g', '\u24D6'], ['h', '\u24D7'], ['i', '\u24D8'], ['j', '\u24D9'], ['k', '\u24DA'], ['l', '\u24DB'], ['m', '\u24DC'],
 	['n', '\u24DD'], ['o', '\u24DE'], ['p', '\u24DF'], ['q', '\u24E0'], ['r', '\u24E1'], ['s', '\u24E2'], ['t', '\u24E3'], ['u', '\u24E4'], ['v', '\u24E5'], ['w', '\u24E6'], ['x', '\u24E7'], ['y', '\u24E8'], ['z', '\u24E9'],
@@ -48,6 +50,18 @@ try {
 	var regdateCache = JSON.parse(fs.readFileSync('config/regdatecache.json', 'utf8'));
 } catch (e) {
 	var regdateCache = {};
+}
+
+try {
+	var urbanCache = JSON.parse(fs.readFileSync('config/udcache.json', 'utf8'));
+} catch (e) {
+	var urbanCache = {};
+}
+
+function cacheUrbanWord (word, definition) {
+	word = word.toLowerCase().replace(/ /g, '');
+	urbanCache[word] = {"definition": definition, "time": Date.now()};
+	fs.writeFile('config/urbancache.json', JSON.stringify(urbanCache));
 }
 
 function cacheRegdate (user, date) {
@@ -317,80 +331,105 @@ var components = exports.components = {
 		return this.sendReply("These users have currently have queued tells: " + Object.keys(tells));
 	},
 
-	u: 'urbandefine',
-	ud: 'urbandefine',
-	urbandefine: function (target, room, user) {
+	urand: 'ud',
+	udrand: 'ud',
+	u: 'ud',
+	ud: function(target, room, user, connection, cmd) {
+		if (!target) {
+			var target = '';
+			var random = true;
+		} else {
+			var random = false;
+		}
+		if (target.toString().length > 50) return this.sendReply('/ud - <phrase> can not be longer than 50 characters.');
 		if (!this.canBroadcast()) return;
-		if (!target) return this.parse('/help urbandefine');
-		if (target > 50) return this.sendReply('Phrase can not be longer than 50 characters.');
+		if (user.userid === 'roseybear' && this.broadcasting) return this.sendReply('lol nope');
 
-		var self = this;
-		var options = {
-			url: 'http://www.urbandictionary.com/iphone/search/define',
-			term: target,
-			headers: {
-				'Referer': 'http://m.urbandictionary.com'
-			},
-			qs: {
-				'term': target
-			}
-		};
+		if (!random) {
+			options = {
+			    url: 'http://www.urbandictionary.com/iphone/search/define',
+			    term: target,
+			    headers: {
+  			    	'Referer': 'http://m.urbandictionary.com'
+   	 			},
+		    	qs: {
+		   	 		'term': target
+   		 		}
+			};
+		} else {
+			options = {
+			    url: 'http://www.urbandictionary.com/iphone/search/random',
+			    headers: {
+  			    	'Referer': 'http://m.urbandictionary.com'
+   	 			},
+			};
+		}
+
+		var milliseconds = ((44640 * 60) * 1000);
+
+		if (urbanCache[target.toLowerCase().replace(/ /g, '')] && Math.round(Math.abs((urbanCache[target.toLowerCase().replace(/ /g, '')].time - Date.now())/(24*60*60*1000))) < 31) {
+			return this.sendReplyBox("<b>" + Tools.escapeHTML(target) + ":</b> " + urbanCache[target.toLowerCase().replace(/ /g, '')].definition.substr(0,400));
+		}
+
+		self = this;
 
 		function callback(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var page = JSON.parse(body);
-				var definitions = page['list'];
-				if (page['result_type'] == 'no_results') {
-					self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
-					return room.update();
-				} else {
-					if (!definitions[0]['word'] || !definitions[0]['definition']) {
-						self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
-						return room.update();
-					}
-					var output = '<b>' + Tools.escapeHTML(definitions[0]['word']) + ':</b> ' + Tools.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' ');
-					if (output.length > 400) output = output.slice(0, 400) + '...';
-					self.sendReplyBox(output);
-					return room.update();
-				}
-			}
+		    if (!error && response.statusCode == 200) {
+		        page = JSON.parse(body);
+		        definitions = page['list'];
+		        if (page['result_type'] == 'no_results') {
+		        	self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
+		        	return room.update();
+		        } else {
+		        	if (!definitions[0]['word'] || !definitions[0]['definition']) {
+		        		self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
+		        		return room.update();
+		        	}
+		        	output = '<b>' + Tools.escapeHTML(definitions[0]['word']) + ':</b> ' + Tools.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' ');
+		        	if (output.length > 400) output = output.slice(0,400) + '...';
+		        	cacheUrbanWord(target, Tools.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' '));
+		        	self.sendReplyBox(output);
+		        	return room.update();
+		        }
+		    }
 		}
-		http.request(options, callback);
+		request(options, callback);
 	},
 
 	def: 'define',
-	define: function (target, room, user) {
-		if (!this.canBroadcast()) return;
-		if (!target) return this.parse('/help define');
+	define: function(target, room, user) {
+		if (!target) return this.sendReply('Usage: /define <word>');
 		target = toId(target);
-		if (target > 50) return this.sendReply('Word can not be longer than 50 characters.');
+		if (target > 50) return this.sendReply('/define <word> - word can not be longer than 50 characters.');
+		if (!this.canBroadcast()) return;
 
-		var self = this;
 		var options = {
-			url: 'http://api.wordnik.com:80/v4/word.json/' + target + '/definitions?limit=3&sourceDictionaries=all' +
-				'&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+		    url: 'http://api.wordnik.com:80/v4/word.json/'+target+'/definitions?limit=3&sourceDictionaries=all' +
+		    '&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
 		};
 
+		var self = this;
+
 		function callback(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var page = JSON.parse(body);
-				var output = '<font color=' + Core.profile.color + '><b>Definitions for ' + target + ':</b></font><br />';
-				if (!page[0]) {
-					self.sendReplyBox('No results for <b>"' + target + '"</b>.');
-					return room.update();
-				} else {
-					var count = 1;
-					for (var u in page) {
-						if (count > 3) break;
-						output += '(' + count + ') ' + page[u]['text'] + '<br />';
-						count++;
-					}
-					self.sendReplyBox(output);
-					return room.update();
-				}
-			}
+		    if (!error && response.statusCode == 200) {
+		        var page = JSON.parse(body);
+		        var output = '<font color=#24678d><b>Definitions for ' + target + ':</b></font><br />';
+		        if (!page[0]) {
+		        	self.sendReplyBox('No results for <b>"' + target + '"</b>.');
+		        	return room.update();
+		        } else {
+		        	var count = 1;
+		        	for (var u in page) {
+		        		if (count > 3) break;
+		        		output += '(<b>'+count+'</b>) ' + Tools.escapeHTML(page[u]['text']) + '<br />';
+		        		count++;
+		        	}
+		        	self.sendReplyBox(output);
+		        	return room.update();
+		        }
+		    }
 		}
-		http.request(options, callback);
+		request(options, callback);
 	},
 	
 	/*********************************************************

a8826667308ec03eb4b993a35d6496eb1edff6f1
panpawn <thebestintheworld12321@gmail.com>
2015-04-18 18:27:42 -0400

    update getbucks

--- commands.js
+++ commands.js
@@ -634,7 +634,7 @@ var commands = exports.commands = {
         this.sendReplyBox('<font size="2"><b>How to get bucks guide:</b><br><ul>' +
             '<li>Play tournaments in the Lobby or any other official room!  These tournaments will always give the winner bucks! Do /tour bucks for more information!</li>' +
             '<li>Sometimes people will do hangmans for money!</li>' +
-            '<li>Casino! Click <button name="joinRoom" value="casino" target="_blank">here</button> to join! In here, everyone gets room voice!</li>' +
+            '<li>Game Chamber Gambling! Click <button name="joinRoom" value="gamechamber" target="_blank">here</button> to join!</li>' +
             '<li>Make a helpful suggestion to the server using /suggest [suggestion] (Bucks may vary)!');
     },
 

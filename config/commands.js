ca7a850471328a41df907ecf5554ff150dc36406
DarkSuicune <ckasslertaub@pclcnj.com>
2015-04-19 18:15:05 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -2457,32 +2457,5 @@ return this.sendReply('You are not set as away.');
 }
  
 user.updateIdentity();
-},
-	hide: 'hideauth',
-	hideauth: function(target, room, user) {
-		if (!this.can('hideauth')) return false;
-		target = target || config.groups.default.global;
-		if (!config.groups.global[target]) {
-			target = config.groups.default.global;
-			this.sendReply("You have picked an invalid group, defaulting to '" + target + "'.");
-		} else if (config.groups.bySymbol[target].globalRank >= config.groups.bySymbol[user.group].globalRank)
-			return this.sendReply("The group you have chosen is either your current group OR one of higher rank. You cannot hide like that.");
-
-		user.getIdentity = function (roomid) {
-			var identity = Object.getPrototypeOf(this).getIdentity.call(this, roomid);
-			if (identity[0] === this.group)
-				return target + identity.slice(1);
-			return identity;
-		};
-		user.updateIdentity();
-		return this.sendReply("You are now hiding your auth as '" + target + "'.");
-	},
-
-	show: 'showauth',
-	showauth: function(target, room, user) {
-		if (!this.can('hideauth')) return false;
-		delete user.getIdentity;
-		user.updateIdentity();
-		return this.sendReply("You are now showing your authority!");
 	},
 };

0c7db11191ce343142dc5fd895e68b3aeca45e6c
panpawn <thebestintheworld12321@gmail.com>
2015-04-21 00:13:29 -0400

    Bibilia's first command to be added

--- chat-plugins/biblia-stuff.js
+++ chat-plugins/biblia-stuff.js
@@ -2,5 +2,17 @@
  * code will be stored. 
 */
 exports.commands = {
-
+	biblia: function(target, room, user, connection) {
+		if (!this.canTalk()) return;
+		if (!this.canBroadcast()) return;
+		var command = toId(target);
+		var reply = "";
+		switch (command) {
+			case 'site':
+			case 'website':
+				reply +=		'Our website can be found <a href="http://thebiblialeague.webs.com/">here.</a>';
+			break;
+		}
+		this.sendReplyBox(reply);
+	}
 };

122db2b02598d9c90a379a23473cf92f853935a7
DarkSuicune <ckasslertaub@pclcnj.com>
2015-04-29 11:32:31 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1430,6 +1430,7 @@ var commands = exports.commands = {
 	/*********************************************************
 	 * Miscellaneous commands
 	 *********************************************************/
+
 	potd: function (target, room, user) {
 		if (!this.can('potd')) return false;
 
@@ -1611,6 +1612,7 @@ var commands = exports.commands = {
 			"got turnip'd by Gamebreaker!",
 			"pissed off Hope!",
 			"became Hope's love slave!",
+			"shipped Broaken Hope!",
 			"felt Gamebreaker's wrath!",
 			"died of a broken heart.",
 			"got scared and left the server!",
@@ -1982,26 +1984,7 @@ var commands = exports.commands = {
 			this.sendReply("For details of a specific command, use something like: /help data");
 		} else if (!matched) {
 			this.sendReply("Help for the command '" + target + "' was not found. Try /help for general help");
-		}hide: 'hideauth',
-                hideauth: function (target, room, user) {
-                if (!this.can('hideauth')) return false;
-                target = target || Config.groups.default.global;
-                if (!Config.groups.global[target]) {
-                        target = Config.groups.default.global;
-                        this.sendReply("You have picked an invalid group, defaulting to '" + target + "'.");
-                } else if (Config.groups.bySymbol[target].globalRank >= Config.groups.bySymbol[user.group].globalRank) {
-                        return this.sendReply("The group you have chosen is either your current group OR one of higher rank. You cannot hide like that.");
-                }
- 
-                user.getIdentity = function (roomid) {
-                        var identity = Object.getPrototypeOf(this).getIdentity.call(this, roomid);
-                        if (identity[0] === this.group)
-                                return target + identity.slice(1);
-                        return identity;
-                };
-                user.updateIdentity();
-                return this.sendReply("You are now hiding your auth as '" + target + "'.");
-        },
+		}
 	},
 	
 		/*********************************************************

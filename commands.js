8f24b1f8dc87620c8c7abb0f9fb20700ea14646b
Anthony <furryblizzard@gmail.com>
2015-05-08 19:30:52 -0400

    Update commands.js

--- commands.js
+++ commands.js
@@ -2331,26 +2331,7 @@ var commands = exports.commands = {
     },
 
 
-    gethex: 'hex',
-    hex: function(target, room, user) {
-        if (!this.canBroadcast()) return;
-        if (!this.canTalk()) return;
-        if (target) {
-            if (toId(target) === 'panpawn') {
-                return this.sendReplyBox('<b><font color="#DA9D01">' + Tools.escapeHTML(target) + '</font></b>.  The hexcode for ' + Tools.escapeHTML(target) + '\'s name color is: #DA9D01.');
-                return;
-            }
-            return this.sendReplyBox('<b><font color="' + hashColor('' + toId(target) + '') + '">' + Tools.escapeHTML(target) + '</font></b>.  The hexcode for ' + Tools.escapeHTML(target) + '\'s name color is: ' + hashColor('' + toId(target) + '') + '.');
-            return;
-        }
-        if (user.userid === 'panpawn') {
-            return this.sendReplyBox('Hello, <b><font color="#DA9D01">' + user.name + '</b></font>.  Your hexcode for your name color is: #DA9D01.');
-            return;
-        } else {
-            return this.sendReplyBox('Hello, <b><font color="' + hashColor('' + toId(user.name) + '') + '">' + user.name + '</font></b>.  Your hexcode for your name color is: ' + hashColor('' + toId(user.name) + '') + '.');
-            return;
-        }
-    },
+    
 
     away: 'afk',
     asleep: 'afk',

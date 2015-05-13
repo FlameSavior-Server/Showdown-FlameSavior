de389a1da268f539b2bb25c35323952f81e0da3d
Anthony <furryblizzard@gmail.com>
2015-05-13 10:01:54 -0400

    add hideauth and showauth.

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -15,6 +15,61 @@ var badges = fs.createWriteStream('badges.txt', {
 });
 
 exports.commands = {
+    
+    hide: 'hideauth',
+    hideauth: function(target, room, user) {
+        if (!user.can('hideauth'))
+            return this.sendReply('/hideauth - access denied.');
+
+        var tar = ' ';
+        if (target) {
+            target = target.trim();
+            if (Config.groupsranking.indexOf(target) > -1 && target != '#') {
+                if (Config.groupsranking.indexOf(target) <= Config.groupsranking.indexOf(user.group)) {
+                    tar = target;
+                } else {
+                    this.sendReply('The group symbol you have tried to use is of a higher authority than you have access to. Defaulting to \' \' instead.');
+                }
+            } else {
+                this.sendReply('You have tried to use an invalid character as your auth symbol. Defaulting to \' \' instead.');
+            }
+        }
+
+        user.getIdentity = function(roomid) {
+            if (!roomid) roomid = 'lobby';
+            if (this.locked) {
+                return '‽' + this.name;
+            }
+            if (this.mutedRooms[roomid]) {
+                return '!' + this.name;
+            }
+            var room = Rooms.rooms[roomid];
+            if (room.auth) {
+                if (room.auth[this.userid]) {
+                    return tar + this.name;
+                }
+                if (this.group !== ' ') return '+' + this.name;
+                return ' ' + this.name;
+            }
+            return tar + this.name;
+        };
+        user.updateIdentity();
+        this.sendReply('You are now hiding your auth symbol as \'' + tar + '\'.');
+        return this.logModCommand(user.name + ' is hiding auth symbol as \'' + tar + '\'');
+    },
+
+    show: 'showauth',
+    showauth: function(target, room, user) {
+        if (!user.can('hideauth'))
+            return this.sendReply('/showauth - access denied.');
+
+        delete user.getIdentity;
+        user.updateIdentity();
+        this.sendReply('You have now revealed your auth symbol.');
+        return this.logModCommand(user.name + ' has revealed their auth symbol.');
+        this.sendReply('Your symbol has been reset.');
+
+    },
     tell: function(target, room, user) {
         if (!this.canTalk()) return;
         if (!target) return this.parse('/help tell');

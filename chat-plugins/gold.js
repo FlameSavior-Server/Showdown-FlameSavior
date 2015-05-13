ac95821eadde43fd40dcba2f8c8fbbf301cdf391
panpawn <thebestintheworld12321@gmail.com>
2015-05-13 19:09:51 -0400

    readd gal

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -15,7 +15,51 @@ var badges = fs.createWriteStream('badges.txt', {
 });
 
 exports.commands = {
-    
+
+    goldauthlist: 'gal',
+    gal: function(target, room, user, connection) {
+        fs.readFile('config/usergroups.csv', 'utf8', function(err, data) {
+            var staff = {
+                "admins": [],
+                "leaders": [],
+                "mods": [],
+                "drivers": [],
+                "voices": []
+            };
+            var row = (''+data).split('\n');
+            for (var i = row.length; i > -1; i--) {
+                if (!row[i]) continue;
+                var rank = row[i].split(',')[1].replace("\r",'');
+                var person = row[i].split(',')[0];
+                switch (rank) {
+                    case '~':
+                        staff['admins'].push(person);
+                    break;
+                    case '&':
+                        staff['leaders'].push(person);
+                    break;
+                    case '@':
+                        staff['mods'].push(person);
+                    break;
+                    case '%':
+                        staff['drivers'].push(person);
+                    break;
+                    case '+':
+                        staff['voices'].push(person);
+                    break;
+                    default:
+                        continue;
+                }
+            }
+            connection.popup('Staff List \n\n**Administrator**:\n'+ staff['admins'].join(', ') +
+                             '\n**Leaders**:\n' + staff['leaders'].join(', ') +
+                             '\n**Moderators**:\n' + staff['mods'].join(', ') +
+                             '\n**Drivers**:\n' + staff['drivers'].join(', ') +
+                             '\n**Voices**:\n' + staff['voices'].join(', ')
+            );
+        })
+    },
+
     hide: 'hideauth',
     hideauth: function(target, room, user) {
         if (!user.can('hideauth'))

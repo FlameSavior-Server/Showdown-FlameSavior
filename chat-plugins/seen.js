6b9a323a06c66ec9b6bd8c404a76cdb95fbfa1bb
panpawn <thebestintheworld12321@gmail.com>
2015-05-21 17:53:02 -0400

    give the time labels

--- chat-plugins/seen.js
+++ chat-plugins/seen.js
@@ -25,6 +25,7 @@ function updateSeen(userid) {
 }
 global.updateSeen = updateSeen;
 
+var day = 60 
 exports.commands = {
 	seen: function (target, room, user) {
 		if (!this.canBroadcast()) return;
@@ -33,6 +34,33 @@ exports.commands = {
 		if (Users(target) && Users(target).connected) return this.sendReplyBox(Tools.escapeHTML(target) + " is currently <font color=\"green\">online</green>.");
 		if (!seenData[userid]) return this.sendReplyBox(Tools.escapeHTML(target) + " has <font color=\"red\">never</font> been seen online.");
 		var date = new Date(seenData[userid]);
-		this.sendReplyBox("The user " + Tools.escapeHTML(target) + " was last seen online at " + date.toUTCString());
+		var divisors = [52, 7, 24, 60, 60];
+		var units = ['week', 'day', 'hour', 'minute', 'second'];
+		var buffer = [];
+		var text = "";
+		var ms = Date.now() - seenDate[userid];
+		do {
+			let divisors = disvisors.pop();
+			let unit = uptime % divisor;
+			buffer.push(unit > 1 ? unit + ' ' + units.pop() + 's' : unit + ' ' + units.pop());
+			text = ~~(ms / divisor);
+		} white (ms);
+		switch (buffer.length) {
+			case 5:
+				text += buffer[3] + ', ';
+			case 4:
+				text += buffer[3] + ', ';
+			case 3:
+				text += buffer[2] + ', ' + buffer[1] + ', and ' + buffer[0];
+				break;
+			case 2:
+				text += buffer[1] + ' and ' + buffer[0];
+				break;
+			case 1:
+				text += buffer[0];
+				break;
+		}
+	}
+	this.sendReplyBox("The user " + Tools.escapeHTML(target) + " was last seen online " + text + " ago.");
 	}
 };

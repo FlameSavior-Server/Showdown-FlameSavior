114ba890fe34dc6e74eeb4bca2e8cbf4ae7a4949
panpawn <thebestintheworld12321@gmail.com>
2015-05-21 16:59:54 -0400

    implement seen 1

---
+++ chat-plugins/seen.js
@@ -0,0 +1,38 @@
+/* Seen command
+ * by jd and panpawn
+ */
+
+var fs = require('fs');
+
+var seenData = {};
+function loadData() {
+	try {
+		seenData = JSON.parse(fs.readFileSync('config/seenData.json', 'utf8'));
+	} catch (e) {
+		seenData = {};
+	}
+}
+loadData();
+
+function saveData() {
+	fs.writeFileSync('config/seenData.json', JSON.stringify(seenData));
+}
+
+function updateSeen(userid) {
+	if (!userid) return false;
+	seenData[toId(userid)] = Date.now();
+	saveData();
+}
+global.updateSeen = updateSeen;
+
+exports.commands = {
+	seen: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		var userid = toId(target);
+		if (userid.length < 1) return this.sendReply("/seen - Please specify a name.");
+		if (Users(target) && Users(target).connected) return this.sendReplyBox(Tools.escapeHTML(target) + " is currently <font color=\"green\">online</green>.");
+		if (!seenData[userid]) return this.sendReplyBox(Tools.escapeHTML(target) + " has <font color=\"red\">never</font> been seen online.");
+		var date = new Date(seenData[userid]);
+		this.sendReplyBox("The user " + Tools.escapeHTML(target) + " was last seen online at " + date.toUTCString());
+	}
+};

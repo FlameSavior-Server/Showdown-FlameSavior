6ca5b0f97ec4aab6582c97884f2b8416e0d69421
panpawn <rocketboy231@gmail.com>
2015-06-01 01:48:24 +0000

    Ban shinies in Gold Battle

--- config/formats.js
+++ config/formats.js
@@ -630,9 +630,8 @@ exports.Formats = [
 		
 		validateSet: function(set) {
 			var template = this.getTemplate(set.species || set.name);
-			if (template.color !== 'Yellow') {
-				return [set.species + " is not a yellow / gold Pokémon."];
-			}
+			if (template.color !== 'Yellow') return [set.species + " is not a yellow / gold Pokémon."];
+			if (set.shiny) return [set.species + " is not allowed to be shiny... This is called Gold Battle, duh."];
 		},
 
 		ruleset: ['Team Preview', 'Sleep Clause Mod', 'Pokemon', 'Standard'],

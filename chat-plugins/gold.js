829ddefe8aea726ac6d16ff410ea15e4e97348ba
Mollete21 <marielaure.sabiude@sfr.fr>
2015-06-13 00:19:41 +0200

    Re-edited afk (fail on the first propose file change)

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -710,6 +710,10 @@ exports.commands = {
 				case 'eating':
 					awayName = user.name + ' - Ⓔⓐⓣⓘⓝⓖ';
 					break;
+				case 'afk':
+					awayName = user.name + ' - ⒶⒻⓀ'
+					break;
+
 				default:
 					awayName = user.name + ' - Ⓐⓦⓐⓨ';
 			}

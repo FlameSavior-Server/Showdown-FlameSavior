5c1f2daf4aa046b7306ea605a0daf35c27f6aea3
panpawn <rocketboy231@gmail.com>
2015-06-19 16:23:38 +0000

    Remove pointless code

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -847,7 +847,6 @@ exports.commands = {
 		}
 	},
 	k: 'kick',
-	aura: 'kick',
 	kick: function(target, room, user) {
 		if (!this.can('lock')) return false;
 		if (!target) return this.sendReply('/help kick');
@@ -861,26 +860,6 @@ exports.commands = {
 		this.addModCommand(targetUser.name + ' was kicked from the room by ' + user.name + '.');
 		targetUser.popup('You were kicked from ' + room.id + ' by ' + user.name + '.');
 		targetUser.leaveRoom(room.id);
-	},
-	dm: 'daymute',
-	daymute: function(target, room, user) {
-		if (!target) return this.parse('/help daymute');
-		if (!this.canTalk()) return false;
-		target = this.splitTarget(target);
-		var targetUser = this.targetUser;
-		if (!targetUser) {
-			return this.sendReply('User ' + this.targetUsername + ' not found.');
-		}
-		if (!this.can('mute', targetUser, room)) return false;
-		if (((targetUser.mutedRooms[room.id] && (targetUser.muteDuration[room.id] || 0) >= 50 * 60 * 1000) || targetUser.locked) && !target) {
-			var problem = ' but was already ' + (!targetUser.connected ? 'offline' : targetUser.locked ? 'locked' : 'muted');
-			return this.privateModCommand('(' + targetUser.name + ' would be muted by ' + user.name + problem + '.)');
-		}
-		targetUser.popup(user.name + ' has muted you for 24 hours. ' + target);
-		this.addModCommand('' + targetUser.name + ' was muted by ' + user.name + ' for 24 hours.' + (target ? " (" + target + ")" : ""));
-		var alts = targetUser.getAlts();
-		if (alts.length) this.addModCommand("" + targetUser.name + "'s alts were also muted: " + alts.join(", "));
-		targetUser.mute(room.id, 24 * 60 * 60 * 1000, true);
 	},
 	flogout: 'forcelogout',
 	forcelogout: function(target, room, user) {

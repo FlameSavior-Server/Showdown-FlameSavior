786350ff7795ab7a2d3c6bb52a9ce80c3e9d6569
panpawn <rocketboy231@gmail.com>
2015-09-15 00:17:28 +0000

    Some improvements with lotto

--- chat-plugins/lottery.js
+++ chat-plugins/lottery.js
@@ -8,6 +8,7 @@ Gold.lottery = {
     ticketPrice: 1, //default ticket price of lottery ticket
     pot: 0,
     gameActive: false,
+    maxTicketsPerUser: 10,
     players: [],
     updatePot: function(amount) {
         this.pot = (this.pot + (amount*2));
@@ -16,15 +17,18 @@ Gold.lottery = {
 
 exports.commands = {
     loto: 'lottery',
+    lotto: 'lottery',
     lottery: function(target, room, user) {
     	var parts = target.split(',');
 		for (var u in parts) parts[u] = parts[u].trim();
+		if (room.id !== 'gamechamber') return this.errorReply("You must be in Game Chamber to use this command.");
 		if (!Rooms.get('gamechamber')) return this.errorReply("You must have the room \"Game Chamber\" in order to use this script.");
         switch (toId(parts[0])) {
             case 'buy':
             case 'join':
                 if (!Gold.lottery.gameActive) return this.errorReply("The game of lottery is not currently running.");
                 if (economy.readMoney(toId(user.name)) < Gold.lottery.ticketPrice) return this.errorReply("You do not have enough bucks to partake in this game of Lottery.  Sorry.");
+                //if (Gold.lottery.maxTicketsPerUser > (Gold.lottery.players.match(toId(user.name)g).length)) return this.errorReply("You cannot get more than " + Gold.lottery.maxTicketsPerUser + " tickets for this game of lotto.");
                 economy.writeMoney('money', toId(user.name), -Gold.lottery.ticketPrice);
                 Gold.lottery.updatePot(Gold.lottery.ticketPrice);
                 Rooms.get('gamechamber').add("|raw|<b><font color=" + Gold.hashColor(toId(user.name)) + ">" + user.name + "</font></b> has bought a lottery ticket.");
@@ -42,13 +46,13 @@ exports.commands = {
                     "<div class=\"broadcast-gold\"><center><b><font size=4 color=red>Lottery Game!</font></b><br />" +
                     "<i><font color=gray>(Started by: " + Tools.escapeHTML(user.name) + ")</font></i><br />" +
                     "A game of lottery has been started!  Cost to join is <b>" + Gold.lottery.ticketPrice + "</b> Gold bucks.<br />" +
-                    "To buy a ticket, do <code>/loto join</code>.</center></div>";
+                    "To buy a ticket, do <code>/lotto join</code>.</center></div>";
                 if (parts[2] === 'pmall') {
                     var loto_notification =
                         "<center><font size=4 color=red><b>Lottery Game!</b></font><br />" +
                         "A game of Lottery has started in <button name=\"send\" value=\"/join gamechamber\">Game Chamber</button>!<br />" +
                         "The ticket cost to join is <b> " + Gold.lottery.ticketPrice + "</b> Gold Bucks.  For every ticket bought, the server automatically matches that price towards the pot.<br />" +
-                        "(For more information, hop in the room and do /loto or ask for help!)</center>";
+                        "(For more information, hop in the room and do /lotto or ask for help!)</center>";
                     var pmName = '~Gold Lottery';
 		            for (var i in Users.users) {
 		            	var message = '|pm|' + pmName + '|' + Users.users[i].getIdentity() + '|/html ' + loto_notification;
@@ -64,7 +68,7 @@ exports.commands = {
                 if (!this.can('hotpatch')) return this.errorReply("Access denied.");
                 if (!Gold.lottery.gameActive) return this.errorReply("There is no active game of lottery currently running.");
                 var winner = Gold.lottery.players[Math.floor(Math.random() * Gold.lottery.players.length)];
-                //if (!winner) Rooms.get('gamechamber').add('|raw|<h2>The game of loto has been canceled due to a lack of players.</h2>');
+                //if (Gold.lottery.players.length = 0) return this.add('|raw|<h2>The game of lotto has been canceled due to a lack of players.</h2>');
                 Rooms.get("gamechamber").add("|raw|<b>" + winner + "</b> has won the game of lottery for " + Gold.lottery.pot + " bucks!</b>");
                 economy.writeMoney('money', toId(winner), Gold.lottery.pot);
                 Gold.lottery.pot = 0;
@@ -82,15 +86,16 @@ exports.commands = {
                 return this.sendReplyBox("The current lottery pot is worth: <b>" + Gold.lottery.pot + "</b> bucks.");
                 break;
             default:
+                if (!this.canBroadcast()) return;
                 this.sendReplyBox(
                     "<center><b>Lottery Commands</b><br />" +
                     "<i><font color=gray>(By: panpawn)</font></i></center><br />" +
-                    "<code>/loto create, [ticket price]</code> - Starts a game of loto with the respected ticket price. (Requires ~)<br />" +
-                    "<code>/loto create, [ticket price], pmall</code> - Starts a game of loto with the respected ticket price AND notifies everyone. (Requires ~)<br />" +
-                    "<code>/loto join</code> OR <code>/loto buy</code> - Buys 1 ticket for the current game of loto (no cap set as of now).<br />" +
-                    "<code>/loto end</code> - Picks a winner of the loto.  (Requires ~)<br />" +
-                    "<code>/loto pot</code> - Shows the current pot of the game of loto.<br />" +
-                    "<code>/loto tickets</code> - Shows all of the current tickets in the current game of loto."
+                    "<code>/lotto create, [ticket price]</code> - Starts a game of lotto with the respected ticket price. (Requires ~)<br />" +
+                    "<code>/lotto create, [ticket price], pmall</code> - Starts a game of lotto with the respected ticket price AND notifies everyone. (Requires ~)<br />" +
+                    "<code>/lotto join</code> OR <code>/loto buy</code> - Buys 1 ticket for the current game of loto (no cap set as of now).<br />" +
+                    "<code>/lotto end</code> - Picks a winner of the lotto.  (Requires ~)<br />" +
+                    "<code>/lotto pot</code> - Shows the current pot of the game of lotto.<br />" +
+                    "<code>/lotto tickets</code> - Shows all of the current tickets in the current game of lotto."
                 );
         }
     }

a8a3308078822f2a78f0c57c19b8c115a77e95bd
Bryan AA <abrayan87@yahoo.com>
2015-09-27 18:12:44 -0600

    Remove unnecessary command
    - This command was repeated two times, so I deleted one.

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -776,28 +776,6 @@ exports.commands = {
 		f
 		this.logEntry(user.name + ' used /gdeclare');
 	},
-	gdeclarered: 'gdeclare',
-	gdeclaregreen: 'gdeclare',
-	gdeclare: function(target, room, user, connection, cmd) {
-		if (!target) return this.parse('/help gdeclare');
-		if (!this.can('lockdown')) return false;
-		var roomName = (room.isPrivate) ? 'a private room' : room.id;
-		if (cmd === 'gdeclare') {
-			for (var id in Rooms.rooms) {
-				if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-blue"><b><font size=1><i>Global declare from ' + roomName + '<br /></i></font size>' + target + '</b></div>');
-			}
-		}
-		if (cmd === 'gdeclarered') {
-			for (var id in Rooms.rooms) {
-				if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-red"><b><font size=1><i>Global declare from ' + roomName + '<br /></i></font size>' + target + '</b></div>');
-			}
-		} else if (cmd === 'gdeclaregreen') {
-			for (var id in Rooms.rooms) {
-				if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-green"><b><font size=1><i>Global declare from ' + roomName + '<br /></i></font size>' + target + '</b></div>');
-			}
-		}
-		this.logModCommand(user.name + ' globally declared ' + target);
-	},
 	declaregreen: 'declarered',
 	declarered: function(target, room, user, connection, cmd) {
 		if (!target) return this.parse('/help declare');

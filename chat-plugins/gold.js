52b87a03d8681f9040a11d954f321497c257935b
panpawn <rocketboy231@gmail.com>
2015-10-02 18:58:45 +0000

    Refactor shitty ATM command This command is already outdated because of /profile, but I figure we can make it look a little more decent than this...

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -1373,69 +1373,25 @@ exports.commands = {
 	purse: 'atm',
 	bag: 'atm',
 	bank: 'atm',
-	atm: function(target, room, user, connection, cmd) {
+	atm: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		var mMatch = false;
-		var money = 0;
-		var total = '';
-		if (!target) {
-			var data = fs.readFileSync('config/money.csv', 'utf8')
-			var row = ('' + data).split("\n");
-			for (var i = row.length; i > -1; i--) {
-				if (!row[i]) continue;
-				var parts = row[i].split(",");
-				var userid = toId(parts[0]);
-				if (user.userid == userid) {
-					var x = Number(parts[1]);
-					var money = x;
-					mMatch = true;
-					if (mMatch === true) {
-						break;
-					}
-				}
+		if (!target) target = user.name;
+		target = toId(target);
+		function output(target) {
+			var bucks = economy.readMoney(target);
+			var label = (bucks == 1 ? ' Gold buck' : ' Gold bucks');
+			var output = "<b>Gold Wallet~</b><br />";
+			target = Tools.escapeHTML(target);
+			switch (bucks) {
+				case 0:
+					output += target + " does not have any Gold bucks.";
+					break;
+				default:
+					output += target + " has " + bucks + label + ".";
 			}
-			if (mMatch === true) {
-				var p = 'Gold bucks';
-				if (money === 1) p = 'Gold buck';
-				total += user.name + ' has ' + money + ' ' + p + '.<br />';
-			}
-			if (mMatch === false) {
-				total += 'You have no Gold bucks.<br />';
-			}
-			user.money = money;
-		} else {
-			var data = fs.readFileSync('config/money.csv', 'utf8')
-			target = this.splitTarget(target);
-			var targetUser = this.targetUser;
-			if (!targetUser) {
-				return this.sendReply('User ' + this.targetUsername + ' not found.');
-			}
-			var money = 0;
-			var row = ('' + data).split("\n");
-			for (var i = row.length; i > -1; i--) {
-				if (!row[i]) continue;
-				var parts = row[i].split(",");
-				var userid = toId(parts[0]);
-				if (targetUser.userid == userid || target == userid) {
-					var x = Number(parts[1]);
-					var money = x;
-					mMatch = true;
-					if (mMatch === true) {
-						break;
-					}
-				}
-			}
-			if (mMatch === true) {
-				var p = 'Gold bucks';
-				if (money < 2) p = 'Gold buck';
-				total += targetUser.name + ' has ' + money + ' ' + p + '.<br />';
-			}
-			if (mMatch === false) {
-				total += targetUser.name + ' has  no Gold bucks.<br />';
-			}
-			targetUser.money = money;
+			return output;
 		}
-		return this.sendReplyBox('<b>Gold Wallet~</b><br>' + total + '');
+		return this.sendReplyBox(output(target));
 	},
 	awardbucks: 'givebucks',
 	gb: 'givebucks',

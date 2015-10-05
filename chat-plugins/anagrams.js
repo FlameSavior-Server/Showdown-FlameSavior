815896396cfe3a1579f559ecc954238c38dcbe4f
panpawn <rocketboy231@gmail.com>
2015-10-05 11:56:15 +0000

    Use this.add(); not room.add() in anagrams end

--- chat-plugins/anagrams.js
+++ chat-plugins/anagrams.js
@@ -49,7 +49,7 @@ exports.commands = {
 				if (!user.can('broadcast', null, room)) return this.sendReply('/endanagram - Access denied.');
 				if (!room.anagram) return this.sendReply('There is no anagram running in here.');
 				delete room.anagram;
-				room.add('|raw|<div class="infobox">The anagram game was ended by <b>' + Tools.escapeHTML(user.name) + '</b></div>');
+				this.add('|raw|<div class="infobox">The anagram game was ended by <b>' + Tools.escapeHTML(user.name) + '</b></div>');
 				break;
 
 			default:

38b6a516f96ba19aa794e3d15f249dd69281069f
panpawn <rocketboy231@gmail.com>
2015-10-21 23:21:03 +0000

    Do not give Guests bucks for voting for polls...

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -29,7 +29,7 @@ var Poll = (function () {
 			return user.sendTo(this.room, "You have already voted for this poll.");
 		} else {
 			this.voters.add(user.latestIp);
-			if (this.room.id === 'lobby') {
+			if (this.room.id === 'lobby' && user.named) {
 				economy.writeMoney('money', user.userid, +1);
 				user.sendTo(this.room, "Thank you for voting for this Lobby poll.  You have received 1 buck as a result!");
 			}

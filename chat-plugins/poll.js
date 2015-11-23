2ab916748a22e476a5a01c1731fd2aef5c10f8ad
panpawn <rocketboy231@gmail.com>
2015-11-23 14:36:33 +0000

    Manually update polls with main server

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -5,8 +5,8 @@
 
 'use strict';
 
-let permission = 'broadcast';
-let blackbutton = 'padding:2px; background-color:#000; border:1px solid #666; color:white';
+const permission = 'broadcast';
+const blackbutton = 'padding:2px; background-color:#000; border:1px solid #666; color:white';
 
 let Poll = (function () {
 	function Poll(room, question, options) {
@@ -17,7 +17,8 @@ let Poll = (function () {
 		}
 		this.room = room;
 		this.question = question;
-		this.voters = new Set();
+		this.voters = {};
+		this.voterIps = {};
 		this.totalVotes = 0;
 		this.timeout = null;
 		this.timeoutMins = 0;
@@ -29,16 +30,15 @@ let Poll = (function () {
 	}
 
 	Poll.prototype.vote = function (user, option) {
-		if (this.voters.has(user.latestIp)) {
+		let ip = user.latestIp;
+		let userid = user.userid;
+
+		if (userid in this.voters || ip in this.voterIps) {
 			return user.sendTo(this.room, "You have already voted for this poll.");
-		} else {
-			this.voters.add(user.latestIp);
-			/*if (this.room.id === 'lobby' && user.named) {
-				economy.writeMoney('money', user.userid, +1);
-				user.sendTo(this.room, "Thank you for voting for this Lobby poll.  You have received 1 buck as a result!");
-			}*/
 		}
 
+		this.voters[userid] = option;
+		this.voterIps[ip] = option;
 		this.options.get(option).votes++;
 		this.totalVotes++;
 
@@ -46,10 +46,14 @@ let Poll = (function () {
 	};
 
 	Poll.prototype.blankvote = function (user, option) {
-		if (this.voters.has(user.latestIp)) {
+		let ip = user.latestIp;
+		let userid = user.userid;
+
+		if (userid in this.voters || ip in this.voterIps) {
 			return user.sendTo(this.room, "You're already looking at the results.");
 		} else {
-			this.voters.add(user.latestIp);
+			this.voters[userid] = 0;
+			this.voterIps[ip] = 0;
 		}
 
 		this.update();
@@ -65,7 +69,7 @@ let Poll = (function () {
 		return output;
 	};
 
-	Poll.prototype.generateResults = function (ended) {
+	Poll.prototype.generateResults = function (ended, option) {
 		let icon = '<span style="border:1px solid #' + (ended ? '777;color:#555' : '6A6;color:#484') + ';border-radius:4px;padding:0 3px"><i class="fa fa-bar-chart"></i> ' + (ended ? "Poll ended" : "Poll") + '</span>';
 		let totalVotes = '<p align="left">[Total Votes: ' + this.totalVotes + ']</p>';
 		let output = '<div class="infobox"><p style="margin: 2px 0 5px 0">' + icon + ' <strong style="font-size:11pt">' + Tools.escapeHTML(this.question) + '</strong></p>' + totalVotes;
@@ -73,10 +77,10 @@ let Poll = (function () {
 
 		let i = iter.next();
 		let c = 0;
-		let colors = ['#8df', '#89d', '#80sd'];
+		let colors = ['#79A', '#8A8', '#88B'];
 		while (!i.done) {
 			let percentage = Math.round((i.value[1].votes * 100) / (this.totalVotes || 1));
-			output += '<div style="margin-top: 3px"><span>' + i.value[0] + '. <strong>' + Tools.escapeHTML(i.value[1].name) + '</strong></span> <small style="font-size:8pt;padding-left:6px"> | ' + percentage + '% (' + i.value[1].votes + ' vote' + (i.value[1].votes === 1 ? '' : 's') + ')</small><br /><span style="background:' + colors[c % 3] + ';padding-right:' + (percentage * 3) + 'px"></span></div>';
+			output += '<div style="margin-top: 3px">' + i.value[0] + '. <strong>' + (i.value[0] === option ? '<em>' : '') + Tools.escapeHTML(i.value[1].name) + (i.value[0] === option ? '</em>' : '') + '</strong> <small> | <small>&nbsp;' + percentage + '%</small> (' + i.value[1].votes + ' vote' + (i.value[1].votes === 1 ? '' : 's') + ')</small><br /><span style="font-size:7pt;background:' + colors[c % 3] + ';padding-right:' + (percentage * 3) + 'px"></span></div>';
 			i = iter.next();
 			c++;
 		}
@@ -86,20 +90,31 @@ let Poll = (function () {
 	};
 
 	Poll.prototype.update = function () {
-		let results = this.generateResults();
+		let results = [];
+
+		for (let i = 0; i <= this.options.size; i++) {
+			results.push(this.generateResults(false, i));
+		}
 
 		// Update the poll results for everyone that has voted
 		for (let i in this.room.users) {
 			let user = this.room.users[i];
-			if (this.voters.has(user.latestIp)) {
-				user.sendTo(this.room, '|uhtmlchange|poll' + this.room.pollNumber + '|' + results);
+			if (user.userid in this.voters) {
+				user.sendTo(this.room, '|uhtmlchange|poll' + this.room.pollNumber + '|' + results[this.voters[user.userid]]);
+			} else if (user.latestIp in this.voterIps) {
+				user.sendTo(this.room, '|uhtmlchange|poll' + this.room.pollNumber + '|' + results[this.voterIps[user.latestIp]]);
 			}
 		}
 	};
 
 	Poll.prototype.display = function (user, broadcast) {
 		let votes = this.generateVotes();
-		let results = this.generateResults();
+
+		let results = [];
+
+		for (let i = 0; i <= this.options.size; i++) {
+			results.push(this.generateResults(false, i));
+		}
 
 		let target = {};
 
@@ -111,8 +126,10 @@ let Poll = (function () {
 
 		for (let i in target) {
 			let thisUser = target[i];
-			if (this.voters.has(thisUser.latestIp)) {
-				thisUser.sendTo(this.room, '|uhtml|poll' + this.room.pollNumber + '|' + results);
+			if (thisUser.userid in this.voters) {
+				thisUser.sendTo(this.room, '|uhtml|poll' + this.room.pollNumber + '|' + results[this.voters[thisUser.userid]]);
+			} else if (thisUser.latestIp in this.voterIps) {
+				thisUser.sendTo(this.room, '|uhtml|poll' + this.room.pollNumber + '|' + results[this.voterIps[thisUser.latestIp]]);
 			} else {
 				thisUser.sendTo(this.room, '|uhtml|poll' + this.room.pollNumber + '|' + votes);
 			}
@@ -146,8 +163,8 @@ exports.commands = {
 				options.push(params[i]);
 			}
 
-			if (options.length > 12) {
-				return this.errorReply("Too many options for poll (maximum is 12).");
+			if (options.length > 8) {
+				return this.errorReply("Too many options for poll (maximum is 8).");
 			}
 
 			room.poll = new Poll(room, params[0], options);
@@ -182,17 +199,14 @@ exports.commands = {
 			if (target) {
 				if (!this.can(permission, null, room)) return false;
 				if (target === 'clear') {
-					if (room.poll.timeout) {
-						clearTimeout(room.poll.timeout);
-						room.poll.timeout = null;
-						room.poll.timeoutMins = 0;
-						return room.add("The timeout for the poll was cleared.");
-					} else {
-						return this.errorReply("No timer to clear.");
-					}
+					if (!room.poll.timeout) return this.errorReply("There is no timer to clear.");
+					clearTimeout(room.poll.timeout);
+					room.poll.timeout = null;
+					room.poll.timeoutMins = 0;
+					return this.add("The timeout for the poll was cleared.");
 				}
 				let timeout = parseFloat(target);
-				if (isNaN(timeout) || timeout <= 0) return this.errorReply("Invalid time given.");
+				if (isNaN(timeout) || timeout <= 0 || timeout > 0x7FFFFFFF) return this.errorReply("Invalid time given.");
 				if (room.poll.timeout) clearTimeout(room.poll.timeout);
 				room.poll.timeoutMins = timeout;
 				room.poll.timeout = setTimeout((function () {
@@ -231,32 +245,6 @@ exports.commands = {
 			return this.privateModCommand("(The poll was ended by " + user.name + ".)");
 		},
 		endhelp: ["/poll end - Ends a poll and displays the results. Requires: % @ # & ~"],
-		
-		timer: function (target, room, user) {
-			if (!this.can(permission, null, room)) return false;
-			if (!room.poll) return this.errorReply("There is no poll running in this room.");
-
-			var timeout = parseFloat(target);
-			if (isNaN(timeout)) return this.errorReply("No time given.");
-			if (room.poll.timeout) clearTimeout(room.poll.timeout);
-			room.poll.timeout = setTimeout((function () {
-				room.poll.end();
-				delete room.poll;
-			}), (timeout * 60000));
-			return this.privateModCommand("(The poll timeout was set to " + timeout + " minutes by " + user.name + ".)");
-		},
-		timerhelp: ["/poll timer [minutes] - Sets the poll to automatically end after [minutes] minutes. Requires: % @ # & ~"],
-
-		pr: 'display',
-		pollremind: 'display',
-		display: function (target, room, user) {
-			if (!room.poll) return this.errorReply("There is no poll running in this room.");
-			if (!this.canBroadcast()) return;
-			room.update();
-
-			room.poll.display(user, this.broadcasting);
-		},
-		displayhelp: ["/poll display - Displays the poll"],
 
 		show: 'display',
 		display: function (target, room, user) {
@@ -269,8 +257,8 @@ exports.commands = {
 		displayhelp: ["/poll display - Displays the poll"],
 
 		'': function (target, room, user) {
-			this.parse('/pollhelp');
-		},
+			this.parse('/help poll');
+		}
 	},
 	pollhelp: function(target, room, user) {
 		if (!this.canBroadcast()) return;
@@ -302,7 +290,7 @@ exports.commands = {
 		if (!this.canBroadcast()) return;
 		room.update();
 
-		room.poll.display(user, this.broadcasting);	
+		room.poll.display(user, this.broadcasting);
 	},
 	tierpoll: 'tpoll',
 	tpoll: function(target, room, user) {

8391864c112af19403e935c39dcec6f97e4b7c8f
panpawn <rocketboy231@gmail.com>
2015-11-27 03:07:21 +0000

    ops

--- chat-plugins/goldradio.js
+++ chat-plugins/goldradio.js
@@ -184,7 +184,7 @@ var commands = {
 		});
 
 		buffer += "Current nominations (" + nominations.length + "):";
-		for (let i = 0; i < nominations.length; i++) {
+		for (var i = 0; i < nominations.length; i++) {
 			buffer += "<br />" +
 				"- " + Tools.escapeHTML(nominations[i][0]) + " (submitted by " + Tools.escapeHTML(nominations[i][1].name) + ")";
 		}

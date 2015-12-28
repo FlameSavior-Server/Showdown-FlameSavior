c6b0e4ee29c62e315895502c81edb25ce7c85a65
panpawn <rocketboy231@gmail.com>
2015-12-28 19:52:54 +0000

    Show settings for being notified in friendslist

--- chat-plugins/friendslist.js
+++ chat-plugins/friendslist.js
@@ -57,7 +57,7 @@ function getAdded(user) {
 	list.forEach(function(kek) {
 		Friends[kek].forEach(function(kek2) {
 			if (user === kek2) {
-				kek = " <font color=" + Gold.hashColor(kek) + ">" + getName(kek) + "</font>";
+				kek = " <button name=\"send\" value=\"/friendslist " + kek + "\"><font color=" + Gold.hashColor(kek) + ">" + getName(kek) + "</font></button>";
 				output.push(kek);
 			}
 		});
@@ -84,7 +84,8 @@ Gold.friendsNotify = friendsNotify;
 
 function formatList(user, by) {
 	if (!Friends[user]) Friends[user] = [];
-	var reply = "<div class=\"infobox-limited\" target=\"_blank\"><b><u>Friends of </u><font color=" + Gold.hashColor(user) + "><u>" + getName(user) + "</u></font> (" + Friends[user].length + "):</b><br />";
+	var reply = "<div class=\"infobox-limited\" target=\"_blank\"><b><u>Friendslist of </u><font color=" + Gold.hashColor(user) + "><u>" + getName(user) + "</u></font> (" + Friends[user].length + "):</b><br />";
+		reply += (NotifySetting[user] ? "(<i>does</i> get notified when friends come online)" : "(<i>does NOT</i> get notified when friends come online)");
 		reply += '<table border="1" cellspacing ="0" cellpadding="3">';
 		reply += "<tr><td><u>Friend:</u></td><td><u>Last Online:</u></td><td><u>Bucks:</u></td></tr>";
 	Friends[user].forEach(function(frens) {

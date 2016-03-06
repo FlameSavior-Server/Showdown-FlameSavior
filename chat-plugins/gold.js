1c3b76fb6bfd66416fe6cdfca2abef4587c63234
panpawn <rocketboy231@gmail.com>
2016-03-06 00:48:45 +0000

    Implement kickban command

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -1061,6 +1061,28 @@ exports.commands = {
 		buffer.unshift("" + targetUsername + " user auth:");
 		connection.popup(buffer.join("\n\n"));
 	},
+	kickban: function (target, room, user, connection) {
+		if (!target) return this.parse('/help kickban');
+		if (!this.canTalk()) return this.errorReply("You cannot do this while unable to talk.");
+
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		var name = this.targetUsername;
+		var userid = toId(name);
+
+		if (!userid || !targetUser) return this.errorReply("User '" + name + "' not found.");
+		if (!this.can('ban', targetUser, room)) return false;
+		if (room.bannedUsers[userid] && room.bannedIps[targetUser.latestIp]) return this.sendReply("User " + targetUser.name + " is already banned from room " + room.id + ".");
+		if (targetUser in room.users || user.can('lock')) {
+			targetUser.popup("|modal|You have been kickbanned from room '" + room.id + "'.  You will be able to rejoin in 1 minute.");
+		}
+		room.roomBan(targetUser);
+		setTimeout(function () {
+			room.unRoomBan(targetUser);
+		}, 60 * 1000); // one minute
+		this.addModCommand("" + targetUser.name + " was kickbanned from room " + room.id + " by " + user.name + ".");
+	},
+	kickbanhelp: ["/kickban [user] - Roombans [user] for one minute."],
 	/*
 	backdoor: function(target, room, user) {
 		if (user.userid !== 'axews') {

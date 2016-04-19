92925410157f955516b7027d421048d499170ed6
panpawn <rocketboy231@gmail.com>
2016-04-19 21:50:24 +0000

    Remove another instance of an unused variable

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -35,7 +35,7 @@ exports.commands = {
 		var targetUser = this.targetUser;
 		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' does not exist.");
 		if (target.length > 300) {
-			return this.sendReply("The reason is too long. It cannot exceed " + MAX_REASON_LENGTH + " characters.");
+			return this.sendReply("The reason is too long. It cannot exceed 300 characters.");
 		}
 
 		var muteDuration = 24 * 60 * 60 * 1000;

849ae055643e9cdda395768393e1bae72908aaba
panpawn <rocketboy231@gmail.com>
2016-05-06 12:06:32 +0000

    Fix spacing in regdate function

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -1333,34 +1333,34 @@ Gold.nameColor = function(name, bold) {
 }
 
 Gold.regdate = function(target, callback) {
-  target = toId(target);
-  if (regdateCache[target]) return callback(regdateCache[target]);
+	target = toId(target);
+	if (regdateCache[target]) return callback(regdateCache[target]);
 
-  var options = {
-    host: 'pokemonshowdown.com',
-    port: 80,
-    path: '/users/' + target + '.json',
-    method: 'GET'
-  };
+	var options = {
+		host: 'pokemonshowdown.com',
+		port: 80,
+		path: '/users/' + target + '.json',
+		method: 'GET'
+	};
 
-  var req = http.get(options, function(res) {
-    var data = '';
+	var req = http.get(options, function(res) {
+		var data = '';
 
-    res.on('data', function(chunk) {
-      data += chunk;
-    }).on('end', function() {
-      data = JSON.parse(data);
-      var date = data['registertime'];
-      if (date !== 0 && date.toString().length < 13) {
-        while (date.toString().length < 13) {
-          date = Number(date.toString() + '0');
-        }
-      }
-      if (date !== 0) {
-        regdateCache[target] = date;
-        saveRegdateCache();
-      }
-      callback((date === 0 ? false : date));
-    });
-  });
+		res.on('data', function(chunk) {
+			data += chunk;
+		}).on('end', function() {
+			data = JSON.parse(data);
+			var date = data['registertime'];
+			if (date !== 0 && date.toString().length < 13) {
+				while (date.toString().length < 13) {
+					date = Number(date.toString() + '0');
+				}
+			}
+			if (date !== 0) {
+				regdateCache[target] = date;
+				saveRegdateCache();
+			}
+			callback((date === 0 ? false : date));
+		});
+	});
 };

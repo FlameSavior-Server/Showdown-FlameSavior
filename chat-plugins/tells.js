f795828da8ff09d1ea1d2b17389eca25a9497ebb
panpawn <rocketboy231@gmail.com>
2016-05-08 15:51:21 +0000

    Fix mistake from removing tabs in tells plugin

--- chat-plugins/tells.js
+++ chat-plugins/tells.js
@@ -42,21 +42,21 @@ function createTell(sender, reciever, message) {
 }
 
 exports.commands = {
-    tell: function (target, room, user) {
-        if (!this.canTalk()) return this.errorReply("You cannot do this while unable to talk.");
-        if (!target) return this.parse('/help tell');
-        let commaIndex = target.indexOf(',');
-        if (commaIndex < 0) return this.errorReply("You forgot the comma.");
-        let targetUser = toId(target.slice(0, commaIndex)), origUser = target.slice(0, commaIndex);
-        let message = target.slice(commaIndex + 1).trim();
-        if (message.length > 500) return this.errorReply("This tell is too large, try making it shorter.");
-        if (targetUser.length < 1 || targetUser.length > 18) return this.errorReply("Usernames cannot be this length.  Check spelling?");
-        if (!message || message.length < 1) return this.errorReply("Tell messages must be at least one character.");
-        if (tells[targetUser] && tells[targetUser].length >= maxTells) return this.errorReply("This user has too many tells queued, try again later.");
-        createTell(user.name, targetUser, message); // function saves when tell is created automatically
-        return this.sendReply("|raw|Your tell to " + Gold.nameColor(origUser, true) + " has been added to their offline messaging queue." + (Users(targetUser) && Users(targetUser).connected && user.userid !== targetUser ? "<br /><b>However, this user is currently online if you would like to private message them.</b>" : ""));
-    },
-    tellhelp: ["/tell [user], [message] - sends a user an offline message to be recieved when they next log on."],
+	tell: function (target, room, user) {
+		if (!this.canTalk()) return this.errorReply("You cannot do this while unable to talk.");
+		if (!target) return this.parse('/help tell');
+		let commaIndex = target.indexOf(',');
+		if (commaIndex < 0) return this.errorReply("You forgot the comma.");
+		let targetUser = toId(target.slice(0, commaIndex)), origUser = target.slice(0, commaIndex);
+		let message = target.slice(commaIndex + 1).trim();
+		if (message.length > 500) return this.errorReply("This tell is too large, try making it shorter.");
+		if (targetUser.length < 1 || targetUser.length > 18) return this.errorReply("Usernames cannot be this length.  Check spelling?");
+		if (!message || message.length < 1) return this.errorReply("Tell messages must be at least one character.");
+		if (tells[targetUser] && tells[targetUser].length >= maxTells) return this.errorReply("This user has too many tells queued, try again later.");
+		createTell(user.name, targetUser, message); // function saves when tell is created automatically
+		return this.sendReply("|raw|Your tell to " + Gold.nameColor(origUser, true) + " has been added to their offline messaging queue." + (Users(targetUser) && Users(targetUser).connected && user.userid !== targetUser ? "<br /><b>However, this user is currently online if you would like to private message them.</b>" : ""));
+	},
+	tellhelp: ["/tell [user], [message] - sends a user an offline message to be recieved when they next log on."],
 };
 
 Gold.tells = tells;

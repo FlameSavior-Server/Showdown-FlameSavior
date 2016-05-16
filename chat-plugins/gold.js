4b7e90d3e66cb212b94c28c4423c0c667d58a330
panpawn <rocketboy231@gmail.com>
2016-05-16 02:02:48 +0000

    Merge chat-plugins/poof.js into chat-plugins/gold.js

--- chat-plugins/gold.js
+++ chat-plugins/gold.js
@@ -1273,6 +1273,40 @@ exports.commands = {
 	coding: function(target, room, user) {
 	    this.parse('/away CODING');
 	},
+	// Poof commands by kota
+	d: 'poof',
+	cpoof: 'poof',
+	poof: function (target, room, user) {
+		if (Config.poofOff) return this.sendReply("Poof is currently disabled.");
+		if (target && !this.can('broadcast')) return false;
+		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
+		if (room.id !== 'lobby') return false;
+		let message = target || messages[Math.floor(Math.random() * messages.length)];
+		if (message.indexOf('{{user}}') < 0) message = '{{user}} ' + message;
+		message = message.replace(/{{user}}/g, user.name);
+		if (!this.canTalk(message)) return false;
+
+		let colour = '#' + [1, 1, 1].map(function () {
+			let part = Math.floor(Math.random() * 0xaa);
+			return (part < 0x10 ? '0' : '') + part.toString(16);
+		}).join('');
+
+		room.addRaw('<center><strong><font color="' + colour + '">~~ ' + Tools.escapeHTML(message) + ' ~~</font></strong></center>');
+		user.lastPoof = Date.now();
+		user.lastPoofMessage = message;
+		user.disconnectAll();
+	},
+	poofoff: 'nopoof',
+	nopoof: function () {
+		if (!this.can('poofoff')) return false;
+		Config.poofOff = true;
+		return this.sendReply("Poof is now disabled.");
+	},
+	poofon: function () {
+		if (!this.can('poofoff')) return false;
+		Config.poofOff = false;
+		return this.sendReply("Poof is now enabled.");
+	},
 	/*
 	pr: 'pollremind',
 	pollremind: function(target, room, user) {
@@ -1418,6 +1452,38 @@ function parseStatus(text, encoding) {
 	}
 	return text;
 }
+const messages = [
+	"ventured into Shrek's Swamp.",
+	"disrespected the OgreLord!",
+	"used Explosion!",
+	"was swallowed up by the Earth!",
+	"was eaten by Lex!",
+	"was sucker punched by Absol!",
+	"has left the building.",
+	"got lost in the woods!",
+	"left for their lover!",
+	"couldn't handle the coldness of Frost!",
+	"was hit by Magikarp's Revenge!",
+	"was sucked into a whirlpool!",
+	"got scared and left the server!",
+	"went into a cave without a repel!",
+	"got eaten by a bunch of piranhas!",
+	"ventured too deep into the forest without an escape rope",
+	"got shrekt",
+	"woke up an angry Snorlax!",
+	"was forced to give jd an oil massage!",
+	"was used as shark bait!",
+	"peered through the hole on Shedinja's back",
+	"received judgment from the almighty Arceus!",
+	"used Final Gambit and missed!",
+	"went into grass without any Pokemon!",
+	"made a Slowbro angry!",
+	"took a focus punch from Breloom!",
+	"got lost in the illusion of reality.",
+	"ate a bomb!",
+	"left for a timeout!",
+	"fell into a snake pit!",
+];
 
 Gold.hasBadge = function(user, badge) {
 	var data = fs.readFileSync('badges.txt', 'utf8');

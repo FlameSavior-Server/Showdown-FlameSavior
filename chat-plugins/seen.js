87b1657dfc1267dc70b5593789cc14b5549ca494
panpawn <rocketboy231@gmail.com>
2016-05-17 22:48:13 +0000

    seen: remove try-catch and new check if target is the user using command

--- chat-plugins/seen.js
+++ chat-plugins/seen.js
@@ -35,26 +35,23 @@ Gold.seenData = seenData;
 exports.commands = {
 	lastseen: 'seen',
 	seen: function (target, room, user) {
-		try {
-			switch (target) {
-			case 'obj':
-				if (!this.runBroadcast()) return;
-				this.sendReplyBox("There have been " + Object.size(seenData) + " user names recorded in this database.");
-				break;
-			default:
-				if (!this.runBroadcast()) return;
-				let userid = toId(target);
-				if (toId(target).length > 18) return this.errorReply("Usernames cannot be over 18 characters.");
-				if (userid.length < 1) return this.errorReply("/seen - Please specify a name.");
-				let userName = '<strong class="username">' + Gold.nameColor(target, false) + '</strong>';
-				if (Users(target) && Users(target).connected) return this.sendReplyBox(userName + ' is currently <font color="green">online</font>.');
-				if (!seenData[userid]) return this.sendReplyBox(userName + ' has <font color=\"red\">never</font> been seen online on this server.');
-				let userLastSeen = moment(seenData[userid]).format("MMMM Do YYYY, h:mm:ss A");
-				this.sendReplyBox(userName + ' was last seen online on ' + userLastSeen + ' EST. (' + moment(seenData[userid]).fromNow() + ')');
-				break;
-			}
-		} catch (e) {
-			return this.sendReply("Something failed: \n" + e.stack);
+		switch (target) {
+		case 'obj':
+			if (!this.runBroadcast()) return;
+			this.sendReplyBox("There have been " + Object.size(seenData) + " user names recorded in this database.");
+			break;
+		default:
+			if (!this.runBroadcast()) return;
+			let userid = toId(target);
+			if (userid.length > 18) return this.errorReply("Usernames cannot be over 18 characters.");
+			if (userid.length < 1) return this.errorReply("/seen - Please specify a name.");
+			let userName = '<strong class="username">' + (Users(target) && Users(target).connected ? Gold.nameColor(Users.getExact(target).name, false) : Gold.nameColor(target, false)) + '</strong>';
+			if (userid === user.userid) return this.sendReplyBox(userName + ", have you looked in a mirror lately?");
+			if (Users(target) && Users(target).connected) return this.sendReplyBox(userName + ' is currently <font color="green">online</font>.');
+			if (!seenData[userid]) return this.sendReplyBox(userName + ' has <font color=\"red\">never</font> been seen online on this server.');
+			let userLastSeen = moment(seenData[userid]).format("MMMM Do YYYY, h:mm:ss A");
+			this.sendReplyBox(userName + ' was last seen online on ' + userLastSeen + ' EST. (' + moment(seenData[userid]).fromNow() + ')');
+			break;
 		}
 	},
 };

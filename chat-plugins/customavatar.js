325d0b26160793ae7cabde82acf360293f17c0fc
panpawn <rocketboy231@gmail.com>
2016-05-21 18:12:07 +0000

    Fix crashes in customavatar from moving it into strict mode

--- chat-plugins/customavatar.js
+++ chat-plugins/customavatar.js
@@ -111,11 +111,11 @@ exports.commands = {
 				}
 				user.avatarCooldown = Date.now();
 
-				let hash = parts[1].trim();
+				hash = parts[1].trim();
 				if (!pendingAdds[hash]) return this.errorReply("Invalid hash.");
 
-				let userid = pendingAdds[hash].userid;
-				let avatar = pendingAdds[hash].avatar;
+				userid = pendingAdds[hash].userid;
+				avatar = pendingAdds[hash].avatar;
 				delete pendingAdds[hash];
 
 				require('child_process').execFile('bash', ['-c', script, '-', avatar, './config/avatars/' + userid], function (e, out, err) {
@@ -140,14 +140,14 @@ exports.commands = {
 
 			case 'remove':
 			case 'delete':
-				let userid = toId(parts[1]);
+				userid = toId(parts[1]);
 				if (!this.can('pban') && Gold.hasBadge(toId(user.name),'vip') && userid !== user.userid) return false;
 				if (!Config.customavatars[userid]) return this.errorReply(userid + " does not have a custom avatar.");
 
 				if (Config.customavatars[userid].toString().split('.').slice(0, -1).join('.') !== userid)
 					return this.errorReply(userid + "'s custom avatar (" + Config.customavatars[userid] + ") cannot be removed with this script.");
 
-				let targetUser = Users.getExact(userid);
+				targetUser = Users.getExact(userid);
 				if (targetUser) targetUser.avatar = 1;
 
 				fs.unlink('./config/avatars/' + Config.customavatars[userid], function (e) {
